changi67/itop

commit 05735a1c212575c39b9695429c3a675681af76f8

(Retrofit from trunk) Rich text editor: the Maximize button icon is missing if  iTop is installed in a directory which name contains spaces
git-svn-id: https://svn.code.sf.net/p/itop/code/branches/2.3@4385 a333f486-631f-4898-b8df-5754b55c2be0

core/inlineimage.class.inc.php
@@ -464,7 +464,7 @@ EOF
 			oEditor.on( 'instanceReady', function() {
 				if(!CKEDITOR.env.iOS && $('#'+oEditor.id+'_toolbox .editor_magnifier').length == 0)
 				{
-					$('#'+oEditor.id+'_toolbox').append('<span class="editor_magnifier" title="$sToggleFullScreen" style="display:block;width:12px;height:11px;border:1px #A6A6A6 solid;cursor:pointer; background-image:url($sAppRootUrl/images/full-screen.png)">&nbsp;</span>');
+					$('#'+oEditor.id+'_toolbox').append('<span class="editor_magnifier" title="$sToggleFullScreen" style="display:block;width:12px;height:11px;border:1px #A6A6A6 solid;cursor:pointer; background-image:url('$sAppRootUrl/images/full-screen.png')">&nbsp;</span>');
 					$('#'+oEditor.id+'_toolbox .editor_magnifier').on('click', function() {
 							oEditor.execCommand('maximize');
 							if ($(this).closest('.cke_maximized').length != 0)

commit 351c7c5efe044959c7c5456429b4b6d41fb4810f

(Retrofit from trunk) Fixes a regression introduced in [r4385] and causing a blank screen when editing an object
git-svn-id: http://svn.code.sf.net/p/itop/code/branches/2.3@4391 a333f486-631f-4898-b8df-5754b55c2be0

core/inlineimage.class.inc.php
@@ -464,7 +464,7 @@ EOF
 			oEditor.on( 'instanceReady', function() {
 				if(!CKEDITOR.env.iOS && $('#'+oEditor.id+'_toolbox .editor_magnifier').length == 0)
 				{
-					$('#'+oEditor.id+'_toolbox').append('<span class="editor_magnifier" title="$sToggleFullScreen" style="display:block;width:12px;height:11px;border:1px #A6A6A6 solid;cursor:pointer; background-image:url('$sAppRootUrl/images/full-screen.png')">&nbsp;</span>');
+					$('#'+oEditor.id+'_toolbox').append('<span class="editor_magnifier" title="$sToggleFullScreen" style="display:block;width:12px;height:11px;border:1px #A6A6A6 solid;cursor:pointer; background-image:url(\\'$sAppRootUrl/images/full-screen.png\\')">&nbsp;</span>');
 					$('#'+oEditor.id+'_toolbox .editor_magnifier').on('click', function() {
 							oEditor.execCommand('maximize');
 							if ($(this).closest('.cke_maximized').length != 0)