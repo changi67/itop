changi67/itop

commit b679794601de8745a65bc8f4f6fab663f0022a5a

Optimization: make it less cumbersome to have the APC user cache enabled for PHP 5.5+. Prior to this enhancement, it used to require a compilation flag (for apcu) until PHP7... then it became mandatory to install a dedicated backward compatibility module named after apcu_bc. Now, having apcu installed and enabled is ENOUGH.
git-svn-id: https://svn.code.sf.net/p/itop/code/trunk@4415 a333f486-631f-4898-b8df-5754b55c2be0

core/apc-compat.php
@@ -1,0 +1,37 @@
+<?php
+// Copyright (C) 2016 Combodo SARL
+//
+//   This file is part of iTop.
+//
+//   iTop is free software; you can redistribute it and/or modify	
+//   it under the terms of the GNU Affero General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   iTop is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU Affero General Public License for more details.
+//
+//   You should have received a copy of the GNU Affero General Public License
+//   along with iTop. If not, see <http://www.gnu.org/licenses/>
+
+if (!function_exists('apc_store') && function_exists('apcu_store'))
+{
+	// Emulate the API of APC, over APCU
+	// Note: for PHP < 7, this compatibility used to be provided by APCU itself (if compiled with some options)
+	//       for PHP 7+, it can be provided by the mean of apcu_bc, which is not so simple to install
+	//       The current emulation aims at skipping this complexity
+	function apc_store($key, $var, $ttl = 0)
+	{
+		return apcu_store($key, $var, $ttl);
+	}
+	function apc_fetch($key)
+	{
+		return apcu_fetch($key);
+	}
+	function apc_delete($key)
+	{
+		return apcu_delete($key);
+	}
+}

core/metamodel.class.php
@@ -22,6 +22,7 @@ require_once(APPROOT.'core/querymodifier.class.inc.php');
 require_once(APPROOT.'core/metamodelmodifier.inc.php');
 require_once(APPROOT.'core/computing.inc.php');
 require_once(APPROOT.'core/relationgraph.class.inc.php');
+require_once(APPROOT.'core/apc-compat.php');
 
 /**
  * Metamodel

commit 615fa010fca89ee75ceef8966b6a02abece7c540

Fixed regression introduced in [r4415]: data model remains unchanged though the customizations have been successfully compiled (and taken into account in the DB). The fix consists in completing the implementation: the verb apc_cache_info was missing, preventing the cache from being reset by the setup. I took the opportunity to add other verbs of apc so as to make sure that the emulation be consistent with the emulation provided by older versions of apcu.
git-svn-id: https://svn.code.sf.net/p/itop/code/trunk@4440 a333f486-631f-4898-b8df-5754b55c2be0

core/apc-compat.php
@@ -16,22 +16,54 @@
 //   You should have received a copy of the GNU Affero General Public License
 //   along with iTop. If not, see <http://www.gnu.org/licenses/>
 
+// Emulate the API of APC, over APCU
+// Note: for PHP < 7, this compatibility used to be provided by APCU itself (if compiled with some options)
+//       for PHP 7+, it can be provided by the mean of apcu_bc, which is not so simple to install
+//       The current emulation aims at skipping this complexity
 if (!function_exists('apc_store') && function_exists('apcu_store'))
 {
-	// Emulate the API of APC, over APCU
-	// Note: for PHP < 7, this compatibility used to be provided by APCU itself (if compiled with some options)
-	//       for PHP 7+, it can be provided by the mean of apcu_bc, which is not so simple to install
-	//       The current emulation aims at skipping this complexity
-	function apc_store($key, $var, $ttl = 0)
+	function apc_add($key, $var, $ttl = 0)
 	{
-		return apcu_store($key, $var, $ttl);
+		return apcu_add($key, $var, $ttl);
+	}
+	function apc_cache_info($cache_type = '', $limited = false)
+	{
+		return apcu_cache_info($limited);
+	}
+	function apc_cas($key, $old, $new)
+	{
+		return apcu_cas($key, $old, $new);
+	}
+	function apc_clear_cache($cache_type = '')
+	{
+		return apcu_clear_cache();
+	}
+	function apc_dec($key, $step = 1, &$success = null)
+	{
+		apcu_dec($key, $step, $success);
+	}
+	function apc_delete($key)
+	{
+		return apcu_delete($key);
+	}
+	function apc_exists($keys)
+	{
+		return apcu_exists($keys);
 	}
 	function apc_fetch($key)
 	{
 		return apcu_fetch($key);
 	}
-	function apc_delete($key)
+	function apc_inc($key, $step = 1, &$success = null)
 	{
-		return apcu_delete($key);
+		apcu_inc($key, $step, $success);
+	}
+	function apc_sma_info($limited = false)
+	{
+		return apcu_sma_info($limited);
+	}
+	function apc_store($key, $var, $ttl = 0)
+	{
+		return apcu_store($key, $var, $ttl);
 	}
 }