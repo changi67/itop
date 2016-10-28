changi67/itop

commit 43078197bb2e33de9a6a7bdfefb17398cde0b128

N°436 Core API: Correctly (mathematically!) handle the "allow all data" flag, with UNIONS and INTERSECTIONS. Requires testing
git-svn-id: https://svn.code.sf.net/p/itop/code/trunk@4404 a333f486-631f-4898-b8df-5754b55c2be0

core/dbobjectsearch.class.php
@@ -35,6 +35,11 @@ class DBObjectSearch extends DBSearch
 	private $m_aPointingTo;
 	private $m_aReferencedBy;
 
+	// By default, some information may be hidden to the current user
+	// But it may happen that we need to disable that feature
+	protected $m_bAllowAllData = false;
+	protected $m_bDataFiltered = false;
+
 	public function __construct($sClass, $sClassAlias = null)
 	{
 		parent::__construct();
@@ -51,6 +56,11 @@ class DBObjectSearch extends DBSearch
 		$this->m_aPointingTo = array();
 		$this->m_aReferencedBy = array();
 	}
+
+	public function AllowAllData($bAllowAllData = true) {$this->m_bAllowAllData = $bAllowAllData;}
+	public function IsAllDataAllowed() {return $this->m_bAllowAllData;}
+	protected function IsDataFiltered() {return $this->m_bDataFiltered; }
+	protected function SetDataFiltered() {$this->m_bDataFiltered = true;}
 
 	// Create a search definition that leads to 0 result, still a valid search object
 	static public function FromEmptySet($sClass)
@@ -654,7 +664,10 @@ class DBObjectSearch extends DBSearch
 
 			$oLeftFilter = $this->DeepClone();
 			$oRightFilter = $oRightFilter->DeepClone();
-	
+
+			$bAllowAllData = ($oLeftFilter->IsAllDataAllowed() && $oRightFilter->IsAllDataAllowed());
+			$oLeftFilter->AllowAllData($bAllowAllData);
+
 			if ($oLeftFilter->GetClass() != $oRightFilter->GetClass())
 			{
 				if (MetaModel::IsParentClass($oLeftFilter->GetClass(), $oRightFilter->GetClass()))
@@ -810,7 +823,7 @@ class DBObjectSearch extends DBSearch
 		return $this->m_oSearchCondition->ApplyParameters(array_merge($this->m_aParams, $aArgs));
 	}
 	
-	public function ToOQL($bDevelopParams = false, $aContextParams = null)
+	public function ToOQL($bDevelopParams = false, $aContextParams = null, $bWithAllowAllFlag = false)
 	{
 		// Currently unused, but could be useful later
 		$bRetrofitParams = false;
@@ -849,6 +862,10 @@ class DBObjectSearch extends DBSearch
 		foreach($this->m_aFullText as $sFullText)
 		{
 			$sRes .= " AND MATCHES '$sFullText'";
+		}
+		if ($bWithAllowAllFlag && $this->m_bAllowAllData)
+		{
+			$sRes .= " ALLOW ALL DATA";
 		}
 		return $sRes;
 	}
@@ -1131,9 +1148,33 @@ class DBObjectSearch extends DBSearch
 
 	public function MakeSQLQuery($aAttToLoad, $bGetCount, $aModifierProperties, $aGroupByExpr = null, $aSelectedClasses = null)
 	{
-		$oBuild = new QueryBuilderContext($this, $aModifierProperties, $aGroupByExpr, $aSelectedClasses);
+		// Hide objects that are not visible to the current user
+		//
+		$oSearch = $this;
+		if (!$this->IsAllDataAllowed() && !$this->IsDataFiltered())
+		{
+			$oVisibleObjects = UserRights::GetSelectFilter($this->GetClass(), $this->GetModifierProperties('UserRightsGetSelectFilter'));
+			if ($oVisibleObjects === false)
+			{
+				// Make sure this is a valid search object, saying NO for all
+				$oVisibleObjects = DBObjectSearch::FromEmptySet($this->GetClass());
+			}
+			if (is_object($oVisibleObjects))
+			{
+				$oVisibleObjects->AllowAllData();
+				$oSearch = $this->Intersect($oVisibleObjects);
+				$oSearch->SetDataFiltered();
+			}
+			else
+			{
+				// should be true at this point, meaning that no additional filtering
+				// is required
+			}
+		}
 
-		$oSQLQuery = $this->MakeSQLObjectQuery($oBuild, $aAttToLoad, array());
+		$oBuild = new QueryBuilderContext($oSearch, $aModifierProperties, $aGroupByExpr, $aSelectedClasses);
+
+		$oSQLQuery = $oSearch->MakeSQLObjectQuery($oBuild, $aAttToLoad, array());
 		$oSQLQuery->SetCondition($oBuild->m_oQBExpressions->GetCondition());
 		if ($aGroupByExpr)
 		{

core/dbsearch.class.php
@@ -42,13 +42,8 @@ abstract class DBSearch
 	const JOIN_POINTING_TO = 0;
 	const JOIN_REFERENCED_BY = 1;
 
-	protected $m_bDataFiltered = false;
 	protected $m_bNoContextParameters = false;
 	protected $m_aModifierProperties = array();
-
-	// By default, some information may be hidden to the current user
-	// But it may happen that we need to disable that feature
-	protected $m_bAllowAllData = false;
 
 	public function __construct()
 	{
@@ -62,12 +57,11 @@ abstract class DBSearch
 		return unserialize(serialize($this)); // Beware this serializes/unserializes the search and its parameters as well
 	}
 
-	public function AllowAllData() {$this->m_bAllowAllData = true;}
-	public function IsAllDataAllowed() {return $this->m_bAllowAllData;}
+	abstract public function AllowAllData();
+	abstract public function IsAllDataAllowed();
+
 	public function NoContextParameters() {$this->m_bNoContextParameters = true;}
 	public function HasContextParameters() {return $this->m_bNoContextParameters;}
-	public function IsDataFiltered() {return $this->m_bDataFiltered; }
-	public function SetDataFiltered() {$this->m_bDataFiltered = true;}
 
 	public function SetModifierProperty($sPluginClass, $sProperty, $value)
 	{
@@ -219,7 +213,7 @@ abstract class DBSearch
         return $oSearchWithAlias;
     }
 
-    abstract public function ToOQL($bDevelopParams = false, $aContextParams = null);
+    abstract public function ToOQL($bDevelopParams = false, $aContextParams = null, $bWithAllowAllFlag = false);
 
 	static protected $m_aOQLQueries = array();
 
@@ -492,39 +486,16 @@ abstract class DBSearch
 
 	protected function GetSQLQuery($aOrderBy, $aArgs, $aAttToLoad, $aExtendedDataSpec, $iLimitCount, $iLimitStart, $bGetCount, $aGroupByExpr = null)
 	{
-		// Hide objects that are not visible to the current user
-		//
-		$oSearch = $this;
-		if (!$this->IsAllDataAllowed() && !$this->IsDataFiltered())
-		{
-			$oVisibleObjects = UserRights::GetSelectFilter($this->GetClass(), $this->GetModifierProperties('UserRightsGetSelectFilter'));
-			if ($oVisibleObjects === false)
-			{
-				// Make sure this is a valid search object, saying NO for all
-				$oVisibleObjects = DBObjectSearch::FromEmptySet($this->GetClass());
-			}
-			if (is_object($oVisibleObjects))
-			{
-				$oSearch = $this->Intersect($oVisibleObjects);
-				$oSearch->SetDataFiltered();
-			}
-			else
-			{
-				// should be true at this point, meaning that no additional filtering
-				// is required
-			}
-		}
-
 		// Compute query modifiers properties (can be set in the search itself, by the context, etc.)
 		//
-		$aModifierProperties = MetaModel::MakeModifierProperties($oSearch);
+		$aModifierProperties = MetaModel::MakeModifierProperties($this);
 
 		// Create a unique cache id
 		//
 		if (self::$m_bQueryCacheEnabled || self::$m_bTraceQueries)
 		{
 			// Need to identify the query
-			$sOqlQuery = $oSearch->ToOql();
+			$sOqlQuery = $this->ToOql(false, null, true);
 
 			if (count($aModifierProperties))
 			{
@@ -553,7 +524,7 @@ abstract class DBSearch
 		}
 		else
 		{
-			$sOqlQuery = "SELECTING... ".$oSearch->GetClass();
+			$sOqlQuery = "SELECTING... ".$this->GetClass();
 			$sOqlId = "query id ? n/a";
 		}
 
@@ -599,7 +570,7 @@ abstract class DBSearch
 		if (!isset($oSQLQuery))
 		{
 			$oKPI = new ExecutionKPI();
-			$oSQLQuery = $oSearch->MakeSQLQuery($aAttToLoad, $bGetCount, $aModifierProperties, $aGroupByExpr);
+			$oSQLQuery = $this->MakeSQLQuery($aAttToLoad, $bGetCount, $aModifierProperties, $aGroupByExpr);
 			$oSQLQuery->SetSourceOQL($sOqlQuery);
 			$oKPI->ComputeStats('MakeSQLQuery', $sOqlQuery);
 
@@ -624,7 +595,7 @@ abstract class DBSearch
 			$aExtendedFields = array();
 			foreach($aExtendedDataSpec['fields'] as $sColumn)
 			{
-				$sColRef = $oSearch->GetClassAlias().'_extdata_'.$sColumn;
+				$sColRef = $this->GetClassAlias().'_extdata_'.$sColumn;
 				$aExtendedFields[$sColRef] = new FieldExpressionResolved($sColumn, $sTableAlias);
 			}
 			$oSQLQueryExt = new SQLObjectQuery($aExtendedDataSpec['table'], $sTableAlias, $aExtendedFields);

core/dbunionsearch.class.php
@@ -55,6 +55,22 @@ class DBUnionSearch extends DBSearch
 		$this->ComputeSelectedClasses();
 	}
 
+	public function AllowAllData()
+	{
+		foreach ($this->aSearches as $oSearch)
+		{
+			$oSearch->AllowAllData();
+		}
+	}
+	public function IsAllDataAllowed()
+	{
+		foreach ($this->aSearches as $oSearch)
+		{
+			if ($oSearch->IsAllDataAllowed() === false) return false;
+		}
+		return true;
+	}
+
 	/**
 	 * Find the lowest common ancestor for each of the selected class
 	 */
@@ -357,12 +373,12 @@ class DBUnionSearch extends DBSearch
 	/**
 	 * Overloads for query building
 	 */ 
-	public function ToOQL($bDevelopParams = false, $aContextParams = null)
+	public function ToOQL($bDevelopParams = false, $aContextParams = null, $bWithAllowAllFlag = false)
 	{
 		$aSubQueries = array();
 		foreach ($this->aSearches as $oSearch)
 		{
-			$aSubQueries[] = $oSearch->ToOQL($bDevelopParams, $aContextParams);
+			$aSubQueries[] = $oSearch->ToOQL($bDevelopParams, $aContextParams, $bWithAllowAllFlag);
 		}
 		$sRet = implode(' UNION ', $aSubQueries);
 		return $sRet;

commit 0c3e7b753a336acef59e8844f0fd6b98b9ef7702

(Retroffit from trunk) N.505 Regression introduced in [r4404]. Security issue - Object visibility totally screwed the APC cache (user data) is enabled. This is a change in the way SQL queries are built and therefore requires testing.
git-svn-id: http://svn.code.sf.net/p/itop/code/branches/2.3@4470 a333f486-631f-4898-b8df-5754b55c2be0

core/dbobjectsearch.class.php
@@ -1366,7 +1366,7 @@ class DBObjectSearch extends DBSearch
 		return $oSQLQuery->RenderUpdate($aScalarArgs);
 	}
 
-	public function MakeSQLQuery($aAttToLoad, $bGetCount, $aModifierProperties, $aGroupByExpr = null, $aSelectedClasses = null)
+	public function GetSQLQueryStructure($aAttToLoad, $bGetCount, $aGroupByExpr = null, $aSelectedClasses = null)
 	{
 		// Hide objects that are not visible to the current user
 		//
@@ -1392,9 +1392,113 @@ class DBObjectSearch extends DBSearch
 			}
 		}
 
-		$oBuild = new QueryBuilderContext($oSearch, $aModifierProperties, $aGroupByExpr, $aSelectedClasses);
+		// Compute query modifiers properties (can be set in the search itself, by the context, etc.)
+		//
+		$aModifierProperties = MetaModel::MakeModifierProperties($oSearch);
 
-		$oSQLQuery = $oSearch->MakeSQLObjectQuery($oBuild, $aAttToLoad, array());
+		// Create a unique cache id
+		//
+		if (self::$m_bQueryCacheEnabled || self::$m_bTraceQueries)
+		{
+			// Need to identify the query
+			$sOqlQuery = $oSearch->ToOql(false, null, true);
+
+			if (count($aModifierProperties))
+			{
+				array_multisort($aModifierProperties);
+				$sModifierProperties = json_encode($aModifierProperties);
+			}
+			else
+			{
+				$sModifierProperties = '';
+			}
+
+			$sRawId = $sOqlQuery.$sModifierProperties;
+			if (!is_null($aAttToLoad))
+			{
+				$sRawId .= json_encode($aAttToLoad);
+			}
+			if (!is_null($aGroupByExpr))
+			{
+				foreach($aGroupByExpr as $sAlias => $oExpr)
+				{
+					$sRawId .= 'g:'.$sAlias.'!'.$oExpr->Render();
+				}
+			}
+			$sRawId .= $bGetCount;
+			$sOqlId = md5($sRawId);
+		}
+		else
+		{
+			$sOqlQuery = "SELECTING... ".$oSearch->GetClass();
+			$sOqlId = "query id ? n/a";
+		}
+
+
+		// Query caching
+		//
+		if (self::$m_bQueryCacheEnabled)
+		{
+			// Warning: using directly the query string as the key to the hash array can FAIL if the string
+			// is long and the differences are only near the end... so it's safer (but not bullet proof?)
+			// to use a hash (like md5) of the string as the key !
+			//
+			// Example of two queries that were found as similar by the hash array:
+			// SELECT SLT JOIN lnkSLTToSLA AS L1 ON L1.slt_id=SLT.id JOIN SLA ON L1.sla_id = SLA.id JOIN lnkContractToSLA AS L2 ON L2.sla_id = SLA.id JOIN CustomerContract ON L2.contract_id = CustomerContract.id WHERE SLT.ticket_priority = 1 AND SLA.service_id = 3 AND SLT.metric = 'TTO' AND CustomerContract.customer_id = 2
+			// and
+			// SELECT SLT JOIN lnkSLTToSLA AS L1 ON L1.slt_id=SLT.id JOIN SLA ON L1.sla_id = SLA.id JOIN lnkContractToSLA AS L2 ON L2.sla_id = SLA.id JOIN CustomerContract ON L2.contract_id = CustomerContract.id WHERE SLT.ticket_priority = 1 AND SLA.service_id = 3 AND SLT.metric = 'TTR' AND CustomerContract.customer_id = 2
+			// the only difference is R instead or O at position 285 (TTR instead of TTO)...
+			//
+			if (array_key_exists($sOqlId, self::$m_aQueryStructCache))
+			{
+				// hit!
+
+				$oSQLQuery = unserialize(serialize(self::$m_aQueryStructCache[$sOqlId]));
+				// Note: cloning is not enough because the subtree is made of objects
+			}
+			elseif (self::$m_bUseAPCCache)
+			{
+				// Note: For versions of APC older than 3.0.17, fetch() accepts only one parameter
+				//
+				$sOqlAPCCacheId = 'itop-'.MetaModel::GetEnvironmentId().'-query-cache-'.$sOqlId;
+				$oKPI = new ExecutionKPI();
+				$result = apc_fetch($sOqlAPCCacheId);
+				$oKPI->ComputeStats('Query APC (fetch)', $sOqlQuery);
+
+				if (is_object($result))
+				{
+					$oSQLQuery = $result;
+					self::$m_aQueryStructCache[$sOqlId] = $oSQLQuery;
+				}
+			}
+		}
+
+		if (!isset($oSQLQuery))
+		{
+			$oKPI = new ExecutionKPI();
+			$oSQLQuery = $oSearch->BuildSQLQueryStruct($aAttToLoad, $bGetCount, $aModifierProperties, $aGroupByExpr);
+			$oKPI->ComputeStats('BuildSQLQueryStruct', $sOqlQuery);
+
+			if (self::$m_bQueryCacheEnabled)
+			{
+				if (self::$m_bUseAPCCache)
+				{
+					$oKPI = new ExecutionKPI();
+					apc_store($sOqlAPCCacheId, $oSQLQuery, self::$m_iQueryCacheTTL);
+					$oKPI->ComputeStats('Query APC (store)', $sOqlQuery);
+				}
+
+				self::$m_aQueryStructCache[$sOqlId] = $oSQLQuery->DeepClone();
+			}
+		}
+		return $oSQLQuery;
+	}
+
+	protected function BuildSQLQueryStruct($aAttToLoad, $bGetCount, $aModifierProperties, $aGroupByExpr = null, $aSelectedClasses = null)
+	{
+		$oBuild = new QueryBuilderContext($this, $aModifierProperties, $aGroupByExpr, $aSelectedClasses);
+
+		$oSQLQuery = $this->MakeSQLObjectQuery($oBuild, $aAttToLoad, array());
 		$oSQLQuery->SetCondition($oBuild->m_oQBExpressions->GetCondition());
 		if ($aGroupByExpr)
 		{

core/dbsearch.class.php
@@ -511,106 +511,8 @@ abstract class DBSearch
 
 	protected function GetSQLQuery($aOrderBy, $aArgs, $aAttToLoad, $aExtendedDataSpec, $iLimitCount, $iLimitStart, $bGetCount, $aGroupByExpr = null)
 	{
-		// Compute query modifiers properties (can be set in the search itself, by the context, etc.)
-		//
-		$aModifierProperties = MetaModel::MakeModifierProperties($this);
-
-		// Create a unique cache id
-		//
-		if (self::$m_bQueryCacheEnabled || self::$m_bTraceQueries)
-		{
-			// Need to identify the query
-			$sOqlQuery = $this->ToOql(false, null, true);
-
-			if (count($aModifierProperties))
-			{
-				array_multisort($aModifierProperties);
-				$sModifierProperties = json_encode($aModifierProperties);
-			}
-			else
-			{
-				$sModifierProperties = '';
-			}
-
-			$sRawId = $sOqlQuery.$sModifierProperties;
-			if (!is_null($aAttToLoad))
-			{
-				$sRawId .= json_encode($aAttToLoad);
-			}
-			if (!is_null($aGroupByExpr))
-			{
-				foreach($aGroupByExpr as $sAlias => $oExpr)
-				{
-					$sRawId .= 'g:'.$sAlias.'!'.$oExpr->Render();
-				}
-			}
-			$sRawId .= $bGetCount;
-			$sOqlId = md5($sRawId);
-		}
-		else
-		{
-			$sOqlQuery = "SELECTING... ".$this->GetClass();
-			$sOqlId = "query id ? n/a";
-		}
-
-
-		// Query caching
-		//
-		if (self::$m_bQueryCacheEnabled)
-		{
-			// Warning: using directly the query string as the key to the hash array can FAIL if the string
-			// is long and the differences are only near the end... so it's safer (but not bullet proof?)
-			// to use a hash (like md5) of the string as the key !
-			//
-			// Example of two queries that were found as similar by the hash array:
-			// SELECT SLT JOIN lnkSLTToSLA AS L1 ON L1.slt_id=SLT.id JOIN SLA ON L1.sla_id = SLA.id JOIN lnkContractToSLA AS L2 ON L2.sla_id = SLA.id JOIN CustomerContract ON L2.contract_id = CustomerContract.id WHERE SLT.ticket_priority = 1 AND SLA.service_id = 3 AND SLT.metric = 'TTO' AND CustomerContract.customer_id = 2
-			// and	
-			// SELECT SLT JOIN lnkSLTToSLA AS L1 ON L1.slt_id=SLT.id JOIN SLA ON L1.sla_id = SLA.id JOIN lnkContractToSLA AS L2 ON L2.sla_id = SLA.id JOIN CustomerContract ON L2.contract_id = CustomerContract.id WHERE SLT.ticket_priority = 1 AND SLA.service_id = 3 AND SLT.metric = 'TTR' AND CustomerContract.customer_id = 2
-			// the only difference is R instead or O at position 285 (TTR instead of TTO)...
-			//
-			if (array_key_exists($sOqlId, self::$m_aQueryStructCache))
-			{
-				// hit!
-
-				$oSQLQuery = unserialize(serialize(self::$m_aQueryStructCache[$sOqlId]));
-				// Note: cloning is not enough because the subtree is made of objects
-			}
-			elseif (self::$m_bUseAPCCache)
-			{
-				// Note: For versions of APC older than 3.0.17, fetch() accepts only one parameter
-				//
-				$sOqlAPCCacheId = 'itop-'.MetaModel::GetEnvironmentId().'-query-cache-'.$sOqlId;
-				$oKPI = new ExecutionKPI();
-				$result = apc_fetch($sOqlAPCCacheId);
-				$oKPI->ComputeStats('Query APC (fetch)', $sOqlQuery);
-
-				if (is_object($result))
-				{
-					$oSQLQuery = $result;
-					self::$m_aQueryStructCache[$sOqlId] = $oSQLQuery;
-				}
-			}
-		}
-
-		if (!isset($oSQLQuery))
-		{
-			$oKPI = new ExecutionKPI();
-			$oSQLQuery = $this->MakeSQLQuery($aAttToLoad, $bGetCount, $aModifierProperties, $aGroupByExpr);
-			$oSQLQuery->SetSourceOQL($sOqlQuery);
-			$oKPI->ComputeStats('MakeSQLQuery', $sOqlQuery);
-
-			if (self::$m_bQueryCacheEnabled)
-			{
-				if (self::$m_bUseAPCCache)
-				{
-					$oKPI = new ExecutionKPI();
-					apc_store($sOqlAPCCacheId, $oSQLQuery, self::$m_iQueryCacheTTL);
-					$oKPI->ComputeStats('Query APC (store)', $sOqlQuery);
-				}
-
-				self::$m_aQueryStructCache[$sOqlId] = $oSQLQuery->DeepClone();
-			}
-		}
+		$oSQLQuery = $this->GetSQLQueryStructure($aAttToLoad, $bGetCount, $aGroupByExpr);
+		$oSQLQuery->SetSourceOQL($this->ToOQL());
 
 		// Join to an additional table, if required...
 		//

core/dbunionsearch.class.php
@@ -456,11 +456,11 @@ class DBUnionSearch extends DBSearch
 		throw new Exception('MakeUpdateQuery is not implemented for the unions!');
 	}
 
-	protected function MakeSQLQuery($aAttToLoad, $bGetCount, $aModifierProperties, $aGroupByExpr = null, $aSelectedClasses = null)
+	protected function GetSQLQueryStructure($aAttToLoad, $bGetCount, $aGroupByExpr = null, $aSelectedClasses = null)
 	{
 		if (count($this->aSearches) == 1)
 		{
-			return $this->aSearches[0]->MakeSQLQuery($aAttToLoad, $bGetCount, $aModifierProperties, $aGroupByExpr);
+			return $this->aSearches[0]->GetSQLQueryStructure($aAttToLoad, $bGetCount, $aGroupByExpr);
 		}
 
 		$aSQLQueries = array();
@@ -515,7 +515,7 @@ class DBUnionSearch extends DBSearch
 					$aQueryGroupByExpr[$sExpressionAlias] = $oExpression->Translate($aTranslationData, false, false);
 				}
 			}
-			$oSubQuery = $oSearch->MakeSQLQuery($aQueryAttToLoad, false, $aModifierProperties, $aQueryGroupByExpr, $aSearchSelectedClasses);
+			$oSubQuery = $oSearch->GetSQLQueryStructure($aQueryAttToLoad, false, $aQueryGroupByExpr, $aSearchSelectedClasses);
 			$aSQLQueries[] = $oSubQuery;
 		}
 