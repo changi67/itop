changi67/itop

commit 809febafa752cddb0ef96b7991b2c33c905f9ad7

N.505 Regression introduced in [r4404]. Security issue - Object visibility totally screwed the APC cache (user data) is enabled. This is a change in the way SQL queries are built and therefore requires testing.
git-svn-id: https://svn.code.sf.net/p/itop/code/trunk@4469 a333f486-631f-4898-b8df-5754b55c2be0

core/dbobjectsearch.class.php
@@ -1366,7 +1366,7 @@ class DBObjectSearch extends DBSearch
 		return $oSQLQuery->RenderUpdate($aScalarArgs);
 	}
 
-	public function MakeSQLQuery($aAttToLoad, $bGetCount, $aModifierProperties, $aGroupByExpr = null, $aSelectedClasses = null)
+	public function GetSQLQueryStructure($aAttToLoad, $bGetCount, $aGroupByExpr = null, $aSelectedClasses = null)
 	{
 		// Hide objects that are not visible to the current user
 		//
@@ -1392,9 +1392,113 @@ class DBObjectSearch extends DBSearch
 			}
 		}
 
-		$oBuild = new QueryBuilderContext($oSearch, $aModifierProperties, $aGroupByExpr, $aSelectedClasses);
+		// Compute query modifiers properties (can be set in the search itself, by the context, etc.)
+		//
+		$aModifierProperties = MetaModel::MakeModifierProperties($oSearch);
 
-		$oSQLQuery = $oSearch->MakeSQLObjectQuery($oBuild, $aAttToLoad, array());
+		// Create a unique cache id
+		//
+		if (self::$m_bQueryCacheEnabled || self::$m_bTraceQueries)
+		{
+			// Need to identify the query
+			$sOqlQuery = $oSearch->ToOql(false, null, true);
+
+			if (count($aModifierProperties))
+			{
+				array_multisort($aModifierProperties);
+				$sModifierProperties = json_encode($aModifierProperties);
+			}
+			else
+			{
+				$sModifierProperties = '';
+			}
+
+			$sRawId = $sOqlQuery.$sModifierProperties;
+			if (!is_null($aAttToLoad))
+			{
+				$sRawId .= json_encode($aAttToLoad);
+			}
+			if (!is_null($aGroupByExpr))
+			{
+				foreach($aGroupByExpr as $sAlias => $oExpr)
+				{
+					$sRawId .= 'g:'.$sAlias.'!'.$oExpr->Render();
+				}
+			}
+			$sRawId .= $bGetCount;
+			$sOqlId = md5($sRawId);
+		}
+		else
+		{
+			$sOqlQuery = "SELECTING... ".$oSearch->GetClass();
+			$sOqlId = "query id ? n/a";
+		}
+
+
+		// Query caching
+		//
+		if (self::$m_bQueryCacheEnabled)
+		{
+			// Warning: using directly the query string as the key to the hash array can FAIL if the string
+			// is long and the differences are only near the end... so it's safer (but not bullet proof?)
+			// to use a hash (like md5) of the string as the key !
+			//
+			// Example of two queries that were found as similar by the hash array:
+			// SELECT SLT JOIN lnkSLTToSLA AS L1 ON L1.slt_id=SLT.id JOIN SLA ON L1.sla_id = SLA.id JOIN lnkContractToSLA AS L2 ON L2.sla_id = SLA.id JOIN CustomerContract ON L2.contract_id = CustomerContract.id WHERE SLT.ticket_priority = 1 AND SLA.service_id = 3 AND SLT.metric = 'TTO' AND CustomerContract.customer_id = 2
+			// and
+			// SELECT SLT JOIN lnkSLTToSLA AS L1 ON L1.slt_id=SLT.id JOIN SLA ON L1.sla_id = SLA.id JOIN lnkContractToSLA AS L2 ON L2.sla_id = SLA.id JOIN CustomerContract ON L2.contract_id = CustomerContract.id WHERE SLT.ticket_priority = 1 AND SLA.service_id = 3 AND SLT.metric = 'TTR' AND CustomerContract.customer_id = 2
+			// the only difference is R instead or O at position 285 (TTR instead of TTO)...
+			//
+			if (array_key_exists($sOqlId, self::$m_aQueryStructCache))
+			{
+				// hit!
+
+				$oSQLQuery = unserialize(serialize(self::$m_aQueryStructCache[$sOqlId]));
+				// Note: cloning is not enough because the subtree is made of objects
+			}
+			elseif (self::$m_bUseAPCCache)
+			{
+				// Note: For versions of APC older than 3.0.17, fetch() accepts only one parameter
+				//
+				$sOqlAPCCacheId = 'itop-'.MetaModel::GetEnvironmentId().'-query-cache-'.$sOqlId;
+				$oKPI = new ExecutionKPI();
+				$result = apc_fetch($sOqlAPCCacheId);
+				$oKPI->ComputeStats('Query APC (fetch)', $sOqlQuery);
+
+				if (is_object($result))
+				{
+					$oSQLQuery = $result;
+					self::$m_aQueryStructCache[$sOqlId] = $oSQLQuery;
+				}
+			}
+		}
+
+		if (!isset($oSQLQuery))
+		{
+			$oKPI = new ExecutionKPI();
+			$oSQLQuery = $oSearch->BuildSQLQueryStruct($aAttToLoad, $bGetCount, $aModifierProperties, $aGroupByExpr);
+			$oKPI->ComputeStats('BuildSQLQueryStruct', $sOqlQuery);
+
+			if (self::$m_bQueryCacheEnabled)
+			{
+				if (self::$m_bUseAPCCache)
+				{
+					$oKPI = new ExecutionKPI();
+					apc_store($sOqlAPCCacheId, $oSQLQuery, self::$m_iQueryCacheTTL);
+					$oKPI->ComputeStats('Query APC (store)', $sOqlQuery);
+				}
+
+				self::$m_aQueryStructCache[$sOqlId] = $oSQLQuery->DeepClone();
+			}
+		}
+		return $oSQLQuery;
+	}
+
+	protected function BuildSQLQueryStruct($aAttToLoad, $bGetCount, $aModifierProperties, $aGroupByExpr = null, $aSelectedClasses = null)
+	{
+		$oBuild = new QueryBuilderContext($this, $aModifierProperties, $aGroupByExpr, $aSelectedClasses);
+
+		$oSQLQuery = $this->MakeSQLObjectQuery($oBuild, $aAttToLoad, array());
 		$oSQLQuery->SetCondition($oBuild->m_oQBExpressions->GetCondition());
 		if ($aGroupByExpr)
 		{

core/dbsearch.class.php
@@ -511,106 +511,8 @@ abstract class DBSearch
 
 	protected function GetSQLQuery($aOrderBy, $aArgs, $aAttToLoad, $aExtendedDataSpec, $iLimitCount, $iLimitStart, $bGetCount, $aGroupByExpr = null)
 	{
-		// Compute query modifiers properties (can be set in the search itself, by the context, etc.)
-		//
-		$aModifierProperties = MetaModel::MakeModifierProperties($this);
-
-		// Create a unique cache id
-		//
-		if (self::$m_bQueryCacheEnabled || self::$m_bTraceQueries)
-		{
-			// Need to identify the query
-			$sOqlQuery = $this->ToOql(false, null, true);
-
-			if (count($aModifierProperties))
-			{
-				array_multisort($aModifierProperties);
-				$sModifierProperties = json_encode($aModifierProperties);
-			}
-			else
-			{
-				$sModifierProperties = '';
-			}
-
-			$sRawId = $sOqlQuery.$sModifierProperties;
-			if (!is_null($aAttToLoad))
-			{
-				$sRawId .= json_encode($aAttToLoad);
-			}
-			if (!is_null($aGroupByExpr))
-			{
-				foreach($aGroupByExpr as $sAlias => $oExpr)
-				{
-					$sRawId .= 'g:'.$sAlias.'!'.$oExpr->Render();
-				}
-			}
-			$sRawId .= $bGetCount;
-			$sOqlId = md5($sRawId);
-		}
-		else
-		{
-			$sOqlQuery = "SELECTING... ".$this->GetClass();
-			$sOqlId = "query id ? n/a";
-		}
-
-
-		// Query caching
-		//
-		if (self::$m_bQueryCacheEnabled)
-		{
-			// Warning: using directly the query string as the key to the hash array can FAIL if the string
-			// is long and the differences are only near the end... so it's safer (but not bullet proof?)
-			// to use a hash (like md5) of the string as the key !
-			//
-			// Example of two queries that were found as similar by the hash array:
-			// SELECT SLT JOIN lnkSLTToSLA AS L1 ON L1.slt_id=SLT.id JOIN SLA ON L1.sla_id = SLA.id JOIN lnkContractToSLA AS L2 ON L2.sla_id = SLA.id JOIN CustomerContract ON L2.contract_id = CustomerContract.id WHERE SLT.ticket_priority = 1 AND SLA.service_id = 3 AND SLT.metric = 'TTO' AND CustomerContract.customer_id = 2
-			// and	
-			// SELECT SLT JOIN lnkSLTToSLA AS L1 ON L1.slt_id=SLT.id JOIN SLA ON L1.sla_id = SLA.id JOIN lnkContractToSLA AS L2 ON L2.sla_id = SLA.id JOIN CustomerContract ON L2.contract_id = CustomerContract.id WHERE SLT.ticket_priority = 1 AND SLA.service_id = 3 AND SLT.metric = 'TTR' AND CustomerContract.customer_id = 2
-			// the only difference is R instead or O at position 285 (TTR instead of TTO)...
-			//
-			if (array_key_exists($sOqlId, self::$m_aQueryStructCache))
-			{
-				// hit!
-
-				$oSQLQuery = unserialize(serialize(self::$m_aQueryStructCache[$sOqlId]));
-				// Note: cloning is not enough because the subtree is made of objects
-			}
-			elseif (self::$m_bUseAPCCache)
-			{
-				// Note: For versions of APC older than 3.0.17, fetch() accepts only one parameter
-				//
-				$sOqlAPCCacheId = 'itop-'.MetaModel::GetEnvironmentId().'-query-cache-'.$sOqlId;
-				$oKPI = new ExecutionKPI();
-				$result = apc_fetch($sOqlAPCCacheId);
-				$oKPI->ComputeStats('Query APC (fetch)', $sOqlQuery);
-
-				if (is_object($result))
-				{
-					$oSQLQuery = $result;
-					self::$m_aQueryStructCache[$sOqlId] = $oSQLQuery;
-				}
-			}
-		}
-
-		if (!isset($oSQLQuery))
-		{
-			$oKPI = new ExecutionKPI();
-			$oSQLQuery = $this->MakeSQLQuery($aAttToLoad, $bGetCount, $aModifierProperties, $aGroupByExpr);
-			$oSQLQuery->SetSourceOQL($sOqlQuery);
-			$oKPI->ComputeStats('MakeSQLQuery', $sOqlQuery);
-
-			if (self::$m_bQueryCacheEnabled)
-			{
-				if (self::$m_bUseAPCCache)
-				{
-					$oKPI = new ExecutionKPI();
-					apc_store($sOqlAPCCacheId, $oSQLQuery, self::$m_iQueryCacheTTL);
-					$oKPI->ComputeStats('Query APC (store)', $sOqlQuery);
-				}
-
-				self::$m_aQueryStructCache[$sOqlId] = $oSQLQuery->DeepClone();
-			}
-		}
+		$oSQLQuery = $this->GetSQLQueryStructure($aAttToLoad, $bGetCount, $aGroupByExpr);
+		$oSQLQuery->SetSourceOQL($this->ToOQL());
 
 		// Join to an additional table, if required...
 		//

core/dbunionsearch.class.php
@@ -456,11 +456,11 @@ class DBUnionSearch extends DBSearch
 		throw new Exception('MakeUpdateQuery is not implemented for the unions!');
 	}
 
-	protected function MakeSQLQuery($aAttToLoad, $bGetCount, $aModifierProperties, $aGroupByExpr = null, $aSelectedClasses = null)
+	protected function GetSQLQueryStructure($aAttToLoad, $bGetCount, $aGroupByExpr = null, $aSelectedClasses = null)
 	{
 		if (count($this->aSearches) == 1)
 		{
-			return $this->aSearches[0]->MakeSQLQuery($aAttToLoad, $bGetCount, $aModifierProperties, $aGroupByExpr);
+			return $this->aSearches[0]->GetSQLQueryStructure($aAttToLoad, $bGetCount, $aGroupByExpr);
 		}
 
 		$aSQLQueries = array();
@@ -515,7 +515,7 @@ class DBUnionSearch extends DBSearch
 					$aQueryGroupByExpr[$sExpressionAlias] = $oExpression->Translate($aTranslationData, false, false);
 				}
 			}
-			$oSubQuery = $oSearch->MakeSQLQuery($aQueryAttToLoad, false, $aModifierProperties, $aQueryGroupByExpr, $aSearchSelectedClasses);
+			$oSubQuery = $oSearch->GetSQLQueryStructure($aQueryAttToLoad, false, $aQueryGroupByExpr, $aSearchSelectedClasses);
 			$aSQLQueries[] = $oSubQuery;
 		}
 

commit 3797fca98d12f22cc002bb675e70adb152b7a5d6

(Retrofit from trunk) N.536 Regression introduced in [r4469] (N.505), itself fixing a regression introduced in [r4404]. REQUIRES TESTING
git-svn-id: https://svn.code.sf.net/p/itop/code/branches/2.3@4489 a333f486-631f-4898-b8df-5754b55c2be0

core/dbobjectsearch.class.php
@@ -1426,6 +1426,7 @@ class DBObjectSearch extends DBSearch
 				}
 			}
 			$sRawId .= $bGetCount;
+			$sRawId .= implode(',', $aSelectedClasses); // Unions may alter the list of selected columns
 			$sOqlId = md5($sRawId);
 		}
 		else
@@ -1476,7 +1477,7 @@ class DBObjectSearch extends DBSearch
 		if (!isset($oSQLQuery))
 		{
 			$oKPI = new ExecutionKPI();
-			$oSQLQuery = $oSearch->BuildSQLQueryStruct($aAttToLoad, $bGetCount, $aModifierProperties, $aGroupByExpr);
+			$oSQLQuery = $oSearch->BuildSQLQueryStruct($aAttToLoad, $bGetCount, $aModifierProperties, $aGroupByExpr, $aSelectedClasses);
 			$oKPI->ComputeStats('BuildSQLQueryStruct', $sOqlQuery);
 
 			if (self::$m_bQueryCacheEnabled)

test/testlist.inc.php
@@ -5130,3 +5130,278 @@ class TestExecActions extends TestBizModel
 		}
 	}
 }
+
+class TestIntersectOptimization extends TestBizModel
+{
+	static public function GetName()
+	{
+		return 'Internal query optimizations (pointing to)';
+	}
+
+	static public function GetDescription()
+	{
+		return 'Clever optimization required for the portal to work fine (expected improvement: query never finishing... to an almost instantaneous query!';
+	}
+
+	protected function DoExecute()
+	{
+		$sBaseQuery = 'SELECT Service AS s JOIN Organization AS o ON s.org_id = o.id WHERE o.name = "The World Company"';
+		$aQueries = array(
+			// Exact same query
+			'SELECT Service AS s JOIN Organization AS o ON s.org_id = o.id WHERE o.name = "The World Company"',
+			// Same query, other aliases
+			'SELECT Service AS s2 JOIN Organization AS o2 ON s2.org_id = o2.id WHERE o2.name = "The World Company"',
+			// Same aliases, different condition
+			'SELECT Service AS s JOIN Organization AS o ON s.org_id = o.id WHERE o.parent_id = 0',
+			// Other aliases, different condition
+			'SELECT Service AS s2 JOIN Organization AS o2 ON s2.org_id = o2.id WHERE o2.parent_id = 0',
+			// Same aliases, simpler query tree
+			'SELECT Service AS s WHERE name LIKE "Save the World"',
+			// Other aliases, simpler query tree
+			'SELECT Service AS s2 WHERE name LIKE "Save the World"',
+			// Same aliases, different query tree
+			'SELECT Service AS s JOIN ServiceFamily AS f ON s.servicefamily_id = f.id WHERE s.org_id = 123 AND f.name = "Care"',
+			// Other aliases, different query tree
+			'SELECT Service AS s2 JOIN ServiceFamily AS f ON s2.servicefamily_id = f.id WHERE s2.org_id = 123 AND f.name = "Care"',
+		);
+		echo "<h4>Base query: ".htmlentities($sBaseQuery, ENT_QUOTES, 'UTF-8')."</h4>\n";
+		foreach ($aQueries as $sOQL)
+		{
+			echo "<h5>Checking: ".htmlentities($sOQL, ENT_QUOTES, 'UTF-8')."</h5>\n";
+			$oSearchA = DBSearch::FromOQL($sBaseQuery);
+			$oSearchB = DBSearch::FromOQL($sOQL);
+			$oIntersect = $oSearchA->Intersect($oSearchB);
+			echo "<p>Intersect: ".htmlentities($oIntersect->ToOQL(), ENT_QUOTES, 'UTF-8')."</p>\n";
+			CMDBSource::TestQuery($oIntersect->MakeSelectQuery());
+			echo "<p>Successfully tested the SQL query.</p>\n";
+		}
+	}
+}
+
+class TestIntersectOptimization2 extends TestBizModel
+{
+	static public function GetName()
+	{
+		return 'Internal query optimizations (referenced by)';
+	}
+
+	static public function GetDescription()
+	{
+		return 'Clever optimization required for the portal to work fine (expected improvement: query never finishing... to an almost instantaneous query!';
+	}
+
+	protected function DoExecute()
+	{
+		$sBaseQuery = 'SELECT Organization AS o JOIN Service AS s ON s.org_id = o.id WHERE s.name = "Help"';
+		$aQueries = array(
+			// Exact same query
+			'SELECT Organization AS o JOIN Service AS s ON s.org_id = o.id WHERE s.name = "Help"',
+			// Same query, other aliases
+			'SELECT Organization AS o2 JOIN Service AS s2 ON s2.org_id = o2.id WHERE s2.name = "Help"',
+			// Same aliases, different condition
+			'SELECT Organization AS o JOIN Service AS s ON s.org_id = o.id WHERE s.servicefamily_id = 321',
+			// Other aliases, different condition
+			'SELECT Organization AS o2 JOIN Service AS s2 ON s2.org_id = o2.id WHERE s2.servicefamily_id = 321',
+			// Same aliases, simpler query tree
+			'SELECT Organization AS o WHERE o.name = "Demo"',
+			// Other aliases, simpler query tree
+			'SELECT Organization AS o2 WHERE o2.name = "Demo"',
+			// Same aliases, different query tree
+			'SELECT Organization AS o JOIN Location AS l ON l.org_id = o.id WHERE l.name = "Paris"',
+			// Other aliases, different query tree
+			'SELECT Organization AS o2 JOIN Location AS l ON l.org_id = o2.id WHERE l.name = "Paris"',
+		);
+		echo "<h4>Base query: ".htmlentities($sBaseQuery, ENT_QUOTES, 'UTF-8')."</h4>\n";
+		foreach ($aQueries as $sOQL)
+		{
+			echo "<h5>Checking: ".htmlentities($sOQL, ENT_QUOTES, 'UTF-8')."</h5>\n";
+			$oSearchA = DBSearch::FromOQL($sBaseQuery);
+			$oSearchB = DBSearch::FromOQL($sOQL);
+			$oIntersect = $oSearchA->Intersect($oSearchB);
+			echo "<p>Intersect: ".htmlentities($oIntersect->ToOQL(), ENT_QUOTES, 'UTF-8')."</p>\n";
+			CMDBSource::TestQuery($oIntersect->MakeSelectQuery());
+			echo "<p>Successfully tested the SQL query.</p>\n";
+		}
+	}
+}
+
+class TestIntersectOptimization3 extends TestBizModel
+{
+	static public function GetName()
+	{
+		return 'Internal query optimizations (mix)';
+	}
+
+	static public function GetDescription()
+	{
+		return 'Clever optimization required for the portal to work fine (expected improvement: query never finishing... to an almost instantaneous query!';
+	}
+
+	protected function DoExecute()
+	{
+		$aQueries = array(
+			array(
+				'SELECT Organization AS o',
+				'SELECT Organization AS o JOIN Location AS l ON l.org_id = o.id JOIN Organization AS p ON o.parent_id = p.id WHERE l.name = "Paris" AND p.code LIKE "toto"',
+			),
+			array(
+				'SELECT UserRequest AS r JOIN Service AS s ON r.service_id = s.id JOIN Organization AS o ON s.org_id = o.id WHERE o.name = "left_name"',
+				'SELECT UserRequest AS r JOIN Service AS s ON r.service_id = s.id JOIN Organization AS o ON s.org_id = o.id WHERE o.name = "right_name"',
+			),
+		);
+		echo "<h4>Mixing....</h4>\n";
+		foreach ($aQueries as $aQ)
+		{
+			$sBaseQuery = $aQ[0];
+			$sOQL = $aQ[1];
+			echo "<h5>Left: ".htmlentities($sBaseQuery, ENT_QUOTES, 'UTF-8')."</h5>\n";
+			echo "<h5>Right: ".htmlentities($sOQL, ENT_QUOTES, 'UTF-8')."</h5>\n";
+			$oSearchA = DBSearch::FromOQL($sBaseQuery);
+			$oSearchB = DBSearch::FromOQL($sOQL);
+			$oIntersect = $oSearchA->Intersect($oSearchB);
+			echo "<p>Intersect: ".htmlentities($oIntersect->ToOQL(), ENT_QUOTES, 'UTF-8')."</p>\n";
+			CMDBSource::TestQuery($oIntersect->MakeSelectQuery());
+			echo "<p>Successfully tested the SQL query.</p>\n";
+		}
+	}
+}
+
+class TestIntersectOptimization4 extends TestBizModel
+{
+	static public function GetName()
+	{
+		return 'Internal query optimizations (Folding on Join/ReferencedBy)';
+	}
+
+	static public function GetDescription()
+	{
+		return 'Clever optimization required for the portal to work fine (expected improvement: query never finishing... to an almost instantaneous query!';
+	}
+
+	protected function DoExecute()
+	{
+		echo "<h4>Here we are (conluding a long series of tests)</h4>\n";
+		$sQueryA = 'SELECT UserRequest AS r JOIN Service AS s ON r.service_id = s.id JOIN Organization AS o ON s.org_id = o.id WHERE r.agent_id = 456 AND s.servicefamily_id = 789 AND o.name = "right_name"';
+		$sQueryB = 'SELECT Service AS s JOIN Organization AS o ON s.org_id = o.id WHERE o.name = "some name"';
+
+		echo "<h5>A: ".htmlentities($sQueryA, ENT_QUOTES, 'UTF-8')."</h5>\n";
+		echo "<h5>B: ".htmlentities($sQueryB, ENT_QUOTES, 'UTF-8')."</h5>\n";
+		$oSearchA = DBSearch::FromOQL($sQueryA);
+		$oSearchB = DBSearch::FromOQL($sQueryB);
+		$oSearchB->AddCondition_ReferencedBy($oSearchA, 'service_id');
+		echo "<p>Referenced by...: ".htmlentities($oSearchB->ToOQL(), ENT_QUOTES, 'UTF-8')."</p>\n";
+		CMDBSource::TestQuery($oSearchB->MakeSelectQuery());
+		echo "<p>Successfully tested the SQL query.</p>\n";
+	}
+}
+
+class TestIntersectOptimization5 extends TestBizModel
+{
+	static public function GetName()
+	{
+		return 'Internal query optimizations (Folding on Join/PointingTo)';
+	}
+
+	static public function GetDescription()
+	{
+		return 'Clever optimization required for the portal to work fine (expected improvement: query never finishing... to an almost instantaneous query!';
+	}
+
+	protected function DoExecute()
+	{
+		echo "<h4>Here we are (conluding a long series of tests)</h4>\n";
+		$sQueryA = 'SELECT Organization AS o JOIN UserRequest AS r ON r.org_id = o.id JOIN Person AS p ON r.caller_id = p.id WHERE o.name LIKE "Company" AND r.service_id = 123 AND p.employee_number LIKE "007"';
+		$sQueryB = 'SELECT UserRequest AS ur JOIN Person AS p ON ur.agent_id = p.id WHERE p.status != "terminated"';
+
+		echo "<h5>A: ".htmlentities($sQueryA, ENT_QUOTES, 'UTF-8')."</h5>\n";
+		echo "<h5>B: ".htmlentities($sQueryB, ENT_QUOTES, 'UTF-8')."</h5>\n";
+		$oSearchA = DBSearch::FromOQL($sQueryA);
+		$oSearchB = DBSearch::FromOQL($sQueryB);
+		$oSearchB->AddCondition_PointingTo($oSearchA, 'org_id');
+		echo "<p>Referenced by...: ".htmlentities($oSearchB->ToOQL(), ENT_QUOTES, 'UTF-8')."</p>\n";
+		CMDBSource::TestQuery($oSearchB->MakeSelectQuery());
+		echo "<p>Successfully tested the SQL query.</p>\n";
+	}
+}
+
+class TestParsingOptimization extends TestBizModel
+{
+	static public function GetName()
+	{
+		return 'Query optimizations (Merging joins on OQL parsing)';
+	}
+
+	static public function GetDescription()
+	{
+		return 'Checking a few queries that do involve query optimizations (implemented for the sake of optimizing the portal)';
+	}
+
+	protected function DoExecute()
+	{
+		$aQueries = array(
+			"SELECT UserRequest AS u
+				JOIN Person AS p1 ON u.caller_id=p1.id
+				JOIN Organization AS o1 ON p1.org_id=o1.id
+				JOIN Person AS p2 ON u.caller_id=p2.id WHERE p2.status='active' AND p1.status='inactive'",
+			"SELECT UserRequest AS u
+				JOIN Person AS p1 ON u.caller_id=p1.id
+				JOIN Person AS p2 ON u.caller_id=p2.id WHERE p2.status='active' AND p1.status='inactive'",
+			"SELECT UserRequest AS u
+				JOIN Person AS p1 ON u.caller_id=p1.id
+				JOIN Organization AS o1 ON p1.org_id=o1.id
+				JOIN Person ON u.caller_id=Person.id
+				JOIN Location AS l ON Person.location_id = l.id WHERE Person.status='active' AND p1.status='inactive' AND l.country='France'",
+		);
+		foreach ($aQueries as $sQuery)
+		{
+			echo "<h5>To Parse: ".htmlentities($sQuery, ENT_QUOTES, 'UTF-8')."</h5>\n";
+			$oSearch = DBSearch::FromOQL($sQuery);
+			$sQueryOpt = $oSearch->ToOQL();
+			echo "<h5>Optimized: ".htmlentities($sQueryOpt, ENT_QUOTES, 'UTF-8')."</h5>\n";
+			CMDBSource::TestQuery($oSearch->MakeSelectQuery());
+			echo "<p>Successfully tested the SQL query.</p>\n";
+		}
+	}
+}
+
+class TestUnions extends TestBizModel
+{
+	static public function GetName()
+	{
+		return 'Unions';
+	}
+
+	static public function GetDescription()
+	{
+		return 'Checking a few UNION queries';
+	}
+
+	protected function DoExecute()
+	{
+		// The two first items did reveal an issue with the query cache,
+		//because SELECT Person on the second line must not give the same query as SELECT Person on the first line
+		$aQueries = array(
+			"SELECT Person UNION SELECT Person" => true,
+			"SELECT Person UNION SELECT Team" => true,
+			"SELECT Person UNION SELECT Contact" => true,
+			"SELECT Contact UNION SELECT Person" => true,
+			"SELECT Person UNION SELECT Organization" => false,
+		);
+		foreach ($aQueries as $sQuery => $bSuccess)
+		{
+			echo "<h5>To Parse: ".htmlentities($sQuery, ENT_QUOTES, 'UTF-8')."</h5>\n";
+			try
+			{
+				$oSearch = DBSearch::FromOQL($sQuery);
+				if (!$bSuccess) throw new Exception('This query should not be parsable!');
+
+				CMDBSource::TestQuery($oSearch->MakeSelectQuery());
+				echo "<p>Successfully tested the SQL query.</p>\n";
+			}
+			catch (OQLException $e)
+			{
+				if ($bSuccess) throw $e;
+			}
+		}
+	}
+}