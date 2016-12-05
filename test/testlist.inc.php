changi67/itop

commit 60b695e124a606927e36bfd08bfaa80c76cc86b8

N.434 ... Continuation... Fixed regression introduced in [r4448]. OQL (parsed) queries are optimized too but the SQL query cannot be executed. See example herebelow: SELECT         UserRequest AS u    JOIN Person AS p1 ON u.caller_id=p1.id    JOIN Organization AS o1 ON p1.org_id=o1.id    JOIN Person ON u.caller_id=Person.id    JOIN Location AS l ON Person.location_id = l.id WHERE Person.status='active' AND p1.status='inactive' AND l.country='France'
git-svn-id: https://svn.code.sf.net/p/itop/code/trunk@4451 a333f486-631f-4898-b8df-5754b55c2be0

core/dbobjectsearch.class.php
@@ -1233,6 +1233,13 @@ class DBObjectSearch extends DBSearch
 
 		$aAliases = array($sClassAlias => $sClass);
 
+		// Note: the condition must be built here, it may be altered later on when optimizing some joins
+		$oConditionTree = $oOqlQuery->GetCondition();
+		if ($oConditionTree instanceof Expression)
+		{
+			$this->m_oSearchCondition = $this->OQLExpressionToCondition($sQuery, $oConditionTree, $aAliases);
+		}
+
 		// Maintain an array of filters, because the flat list is in fact referring to a tree
 		// And this will be an easy way to dispatch the conditions
 		// $this will be referenced by the other filters, or the other way around...
@@ -1241,19 +1248,32 @@ class DBObjectSearch extends DBSearch
 		$aJoinSpecs = $oOqlQuery->GetJoins();
 		if (is_array($aJoinSpecs))
 		{
+			$aAliasTranslation = array();
 			foreach ($aJoinSpecs as $oJoinSpec)
 			{
 				$sJoinClass = $oJoinSpec->GetClass();
 				$sJoinClassAlias = $oJoinSpec->GetClassAlias();
+				if (isset($aAliasTranslation[$sJoinClassAlias]['*']))
+				{
+					$sJoinClassAlias = $aAliasTranslation[$sJoinClassAlias]['*'];
+				}
 
 				// Assumption: ext key on the left only !!!
 				// normalization should take care of this
 				$oLeftField = $oJoinSpec->GetLeftField();
 				$sFromClass = $oLeftField->GetParent();
+				if (isset($aAliasTranslation[$sFromClass]['*']))
+				{
+					$sFromClass = $aAliasTranslation[$sFromClass]['*'];
+				}
 				$sExtKeyAttCode = $oLeftField->GetName();
 
 				$oRightField = $oJoinSpec->GetRightField();
 				$sToClass = $oRightField->GetParent();
+				if (isset($aAliasTranslation[$sToClass]['*']))
+				{
+					$sToClass = $aAliasTranslation[$sToClass]['*'];
+				}
 
 				$aAliases[$sJoinClassAlias] = $sJoinClass;
 				$aJoinItems[$sJoinClassAlias] = new DBObjectSearch($sJoinClass, $sJoinClassAlias);
@@ -1295,19 +1315,16 @@ class DBObjectSearch extends DBSearch
 				{
 					$oReceiver = $aJoinItems[$sToClass];
 					$oNewComer = $aJoinItems[$sFromClass];
-
-					$aAliasTranslation = array();
 					$oReceiver->AddCondition_ReferencedBy_InNameSpace($oNewComer, $sExtKeyAttCode, $oReceiver->m_aClasses, $aAliasTranslation, $iOperatorCode);
 				}
 				else
 				{
 					$oReceiver = $aJoinItems[$sFromClass];
 					$oNewComer = $aJoinItems[$sToClass];
-
-					$aAliasTranslation = array();
 					$oReceiver->AddCondition_PointingTo_InNameSpace($oNewComer, $sExtKeyAttCode, $oReceiver->m_aClasses, $aAliasTranslation, $iOperatorCode);
 				}
 			}
+			$this->m_oSearchCondition = $this->m_oSearchCondition->Translate($aAliasTranslation, false, false /* leave unresolved fields */);
 		}
 
 		// Check and prepare the select information
@@ -1318,12 +1335,6 @@ class DBObjectSearch extends DBSearch
 			$this->m_aSelectedClasses[$sClassToSelect] = $aAliases[$sClassToSelect];
 		}
 		$this->m_aClasses = $aAliases;
-
-		$oConditionTree = $oOqlQuery->GetCondition();
-		if ($oConditionTree instanceof Expression)
-		{
-			$this->m_oSearchCondition = $this->OQLExpressionToCondition($sQuery, $oConditionTree, $aAliases);
-		}
 	}
 
 	////////////////////////////////////////////////////////////////////////////

commit 09be78df831809629cbef5c498cea7d031294cd9

(Retrofit from trunk) N.539 Regression introduced in [r4451] on oct 7th. Some OQL were issuing a notice and some were generating a SQL query that would fail with error "Column 'functionalci_id' in where clause is ambiguous" (See CI details)
git-svn-id: https://svn.code.sf.net/p/itop/code/branches/2.3@4493 a333f486-631f-4898-b8df-5754b55c2be0

core/dbobjectsearch.class.php
@@ -1237,7 +1237,16 @@ class DBObjectSearch extends DBSearch
 		$oConditionTree = $oOqlQuery->GetCondition();
 		if ($oConditionTree instanceof Expression)
 		{
-			$this->m_oSearchCondition = $this->OQLExpressionToCondition($sQuery, $oConditionTree, $aAliases);
+			$aRawAliases = array($sClassAlias => $sClass);
+			$aJoinSpecs = $oOqlQuery->GetJoins();
+			if (is_array($aJoinSpecs))
+			{
+				foreach ($aJoinSpecs as $oJoinSpec)
+				{
+					$aRawAliases[$oJoinSpec->GetClassAlias()] = $oJoinSpec->GetClass();
+				}
+			}
+			$this->m_oSearchCondition = $this->OQLExpressionToCondition($sQuery, $oConditionTree, $aRawAliases);
 		}
 
 		// Maintain an array of filters, because the flat list is in fact referring to a tree

test/testlist.inc.php
@@ -5401,6 +5401,55 @@ class TestUnions extends TestBizModel
 			catch (OQLException $e)
 			{
 				if ($bSuccess) throw $e;
+				echo "<p>Failed as expected.</p>\n";
+			}
+		}
+	}
+}
+
+class TestImplicitAlias extends TestBizModel
+{
+	static public function GetName()
+	{
+		return 'OQLImplicitAliases';
+	}
+
+	static public function GetDescription()
+	{
+		return 'Checking implicit aliases resolution';
+	}
+
+	protected function DoExecute()
+	{
+		// The two first items did reveal an issue with the query cache,
+		//because SELECT Person on the second line must not give the same query as SELECT Person on the first line
+		$aQueries = array(
+			"SELECT Person WHERE org_id = 1" => true,
+			"SELECT Person WHERE s.org_id = 1" => false,
+			"SELECT Person AS p WHERE p.org_id = 1" => true,
+			"SELECT Person AS p WHERE Person.org_id = 1" => false,
+			"SELECT P FROM Organization AS O JOIN Person AS P ON P.org_id = O.id WHERE org_id = 2" => true, // Bug N.539
+			"SELECT Server JOIN Location ON Server.location_id = Location.id" => true,
+			"SELECT Server JOIN Location ON Server.location_id = id" => false,
+			"SELECT Server JOIN Location ON Server = Location.id" => false,
+			"SELECT Server JOIN Location ON Server.location_id = Location.id WHERE Server.org_id = 1" => true,
+			"SELECT Server JOIN Location ON Server.location_id = Location.id WHERE org_id = 1" => false,
+		);
+		foreach ($aQueries as $sQuery => $bSuccess)
+		{
+			echo "<h5>To Parse: ".htmlentities($sQuery, ENT_QUOTES, 'UTF-8')."</h5>\n";
+			try
+			{
+				$oSearch = DBSearch::FromOQL($sQuery);
+				if (!$bSuccess) throw new Exception('This query should not be parsable!');
+
+				CMDBSource::TestQuery($oSearch->MakeSelectQuery());
+				echo "<p>Successfully tested the SQL query.</p>\n";
+			}
+			catch (OQLException $e)
+			{
+				if ($bSuccess) throw $e;
+				echo "<p>Failed as expected.</p>\n";
 			}
 		}
 	}