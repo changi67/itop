changi67/itop

commit c4cd272cfc3a5e525646835037bae22c7cb058de

N.858 (partial) Allow administrators to access the Admin tools menu even in Read-only modes (access_mode or Archive mode). Still, the Data administration menu gets hidden in Read-only modes.
git-svn-id: https://svn.code.sf.net/p/itop/code/trunk@4716 a333f486-631f-4898-b8df-5754b55c2be0

synchro/synchrodatasource.class.inc.php
@@ -2922,8 +2922,8 @@ class SynchroExecution
 	}
 }
 
-	$oAdminMenu = new MenuGroup('AdminTools', 80 /* fRank */, 'SynchroDataSource', UR_ACTION_MODIFY, UR_ALLOWED_YES);
-	new OQLMenuNode('DataSources', 'SELECT SynchroDataSource', $oAdminMenu->GetIndex(), 12 /* fRank */, true, 'SynchroDataSource', UR_ACTION_MODIFY, UR_ALLOWED_YES);
-//	new OQLMenuNode('Replicas', 'SELECT SynchroReplica', $oAdminMenu->GetIndex(), 12 /* fRank */, true, 'SynchroReplica', UR_ACTION_MODIFY, UR_ALLOWED_YES);
-//	new WebPageMenuNode('Test:RunSynchro', '../synchro/synchro_exec.php', $oAdminMenu->GetIndex(), 13 /* fRank */, 'SynchroDataSource');
-?>
+if (UserRights::IsAdministrator())
+{
+	$oAdminMenu = new MenuGroup('AdminTools', 80 /* fRank */);
+	new OQLMenuNode('DataSources', 'SELECT SynchroDataSource', $oAdminMenu->GetIndex(), 12 /* fRank */, true);
+}

commit 392863b77fb516c2690d0ada125fc9d5c71a3324

N°858.2 Fixed regression introduced in r4716 making DataSynchro menu hidden from the Admin menu
git-svn-id: https://svn.code.sf.net/p/itop/code/trunk@4879 a333f486-631f-4898-b8df-5754b55c2be0

datamodels/2.x/itop-welcome-itil/main.itop-welcome-itil.php
@@ -57,6 +57,7 @@ class ItopWelcome extends ModuleHandlerAPI
 			new WebPageMenuNode('ExportMenu', utils::GetAbsoluteUrlAppRoot().'webservices/export-v2.php?interactive=1', $oAdminMenu->GetIndex(), 9 /* fRank */);
 			new WebPageMenuNode('DataModelMenu', utils::GetAbsoluteUrlAppRoot().'pages/schema.php', $oAdminMenu->GetIndex(), 10 /* fRank */);
 			new WebPageMenuNode('UniversalSearchMenu', utils::GetAbsoluteUrlAppRoot().'pages/UniversalSearch.php', $oAdminMenu->GetIndex(), 11 /* fRank */);
+            new OQLMenuNode('DataSources', 'SELECT SynchroDataSource', $oAdminMenu->GetIndex(), 12 /* fRank */, true);
 		}
 	}
 }

synchro/synchrodatasource.class.inc.php
@@ -2919,9 +2919,3 @@ class SynchroExecution
 		return false;
 	}
 }
-
-if (UserRights::IsAdministrator())
-{
-	$oAdminMenu = new MenuGroup('AdminTools', 80 /* fRank */);
-	new OQLMenuNode('DataSources', 'SELECT SynchroDataSource', $oAdminMenu->GetIndex(), 12 /* fRank */, true);
-}