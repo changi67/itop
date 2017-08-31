changi67/itop

commit 9bfdbfd235fe7153e6aa8177b86bf5db529b94fd

N°583 CSV Import now supports friendlynames as reconciliation keys. More over, read-only attributes (friendlyname, obsolescence flag, ...) are forced to reconciliation scheme.
git-svn-id: https://svn.code.sf.net/p/itop/code/trunk@4861 a333f486-631f-4898-b8df-5754b55c2be0

core/bulkchange.class.inc.php
@@ -470,6 +470,8 @@ class BulkChange
 		{
 			// skip the private key, if any
 			if ($sAttCode == 'id') continue;
+			// skip reconciliation keys
+			if (in_array($sAttCode, $this->m_aReconcilKeys)) continue;
 
 			$oAttDef = MetaModel::GetAttributeDef($this->m_sClass, $sAttCode);
 			$aReasons = array();

pages/ajax.csvimport.php
@@ -149,7 +149,8 @@ function GetMappingForField($sClassName, $sFieldName, $iFieldIndex, $bAdvancedMo
 			$sTargetClass = $oAttDef->GetTargetClass();
 			foreach(MetaModel::ListAttributeDefs($sTargetClass) as $sTargetAttCode => $oTargetAttDef)
 			{
-				if (MetaModel::IsReconcKey($sTargetClass, $sTargetAttCode))
+				// Note: Could not use "MetaModel::GetFriendlyNameAttributeCode($sTargetClass) === $sTargetAttCode" as it would return empty because the friendlyname is composite.
+				if (MetaModel::IsReconcKey($sTargetClass, $sTargetAttCode) || ($oTargetAttDef instanceof AttributeFriendlyName))
 				{
 					$bExtKey = $oTargetAttDef->IsExternalKey();
 					$aSignatures = array();
@@ -176,14 +177,17 @@ function GetMappingForField($sClassName, $sFieldName, $iFieldIndex, $bAdvancedMo
 				}
 			}
 		}
-		else if ($oAttDef->IsWritable() && (!$oAttDef->IsLinkset() || ($bAdvancedMode && $oAttDef->IsIndirect())))
+		else if (
+			($oAttDef->IsWritable() && (!$oAttDef->IsLinkset() || ($bAdvancedMode && $oAttDef->IsIndirect())))
+			|| ($oAttDef instanceof AttributeFriendlyName)
+		)
 		{
 			$aChoices[$sAttCode] = MetaModel::GetLabel($sClassName, $sAttCode, true);
 			if ( ($sFieldName == $oAttDef->GetLabel()) || ($sFieldName == $sAttCode))
 			{
 				$sFieldCode = $sAttCode;
 			}
-		}		
+		}
 	}
 	asort($aChoices);
 
@@ -412,10 +416,23 @@ try
 				}
 				$sDefaultKeys = '"'.implode('", "', $aDefaultKeys).'"';
 			}
+
+			// Read only attributes (will be forced to "search")
+			$aReadOnlyKeys = array();
+            foreach(MetaModel::ListAttributeDefs($sClassName) as $sAttCode => $oAttDef)
+            {
+				if(!$oAttDef->IsWritable())
+				{
+					$aReadOnlyKeys[] = $sAttCode;
+				}
+            }
+            $sReadOnlyKeys = '"'.implode('", "', $aReadOnlyKeys).'"';
+
 			$oPage->add_ready_script(
 <<<EOF
 		$('select[name^=field]').change( DoCheckMapping );
 		aDefaultKeys = new Array($sDefaultKeys);
+		aReadOnlyKeys = new Array($sReadOnlyKeys);
 		DoCheckMapping();
 EOF
 );	

pages/csvimport.php
@@ -835,6 +835,7 @@ EOF
 		$oPage->add_script(
 <<<EOF
 	var aDefaultKeys = new Array();
+	var aReadOnlyKeys = new Array();
 	
 	function CSVGoBack()
 	{
@@ -1003,6 +1004,12 @@ EOF
 			{
 				// Non-mapped field, uncheck and disabled
 				$('#search_'+index).attr('checked', false);
+				$('#search_'+index).attr('disabled', true);
+			}
+			else if (aReadOnlyKeys.indexOf(sMappingValue) >= 0)
+			{
+				// Read-only attribute forced to reconciliation key
+				$('#search_'+index).attr('checked', true);
 				$('#search_'+index).attr('disabled', true);
 			}
 			else if (index == idSelected)

commit 04cf53d8593c1346cefa59339208ffd95f37943d

N°583.2 Fixed regression introduced in r4861.
git-svn-id: https://svn.code.sf.net/p/itop/code/trunk@4885 a333f486-631f-4898-b8df-5754b55c2be0

core/bulkchange.class.inc.php
@@ -1,5 +1,5 @@
 <?php
-// Copyright (C) 2010-2015 Combodo SARL
+// Copyright (C) 2010-2017 Combodo SARL
 //
 //   This file is part of iTop.
 //
@@ -468,10 +468,12 @@ class BulkChange
 		//
 		foreach ($this->m_aAttList as $sAttCode => $iCol)
 		{
+            $oAttDef = MetaModel::GetAttributeDef($this->m_sClass, $sAttCode);
+
 			// skip the private key, if any
 			if ($sAttCode == 'id') continue;
 			// skip reconciliation keys
-			if (in_array($sAttCode, $this->m_aReconcilKeys)) continue;
+			if (!$oAttDef->IsWritable() && in_array($sAttCode, $this->m_aReconcilKeys)){ continue; }
 
 			$oAttDef = MetaModel::GetAttributeDef($this->m_sClass, $sAttCode);
 			$aReasons = array();
@@ -1308,5 +1310,3 @@ EOF
 	}	
 }
 
-
-?>