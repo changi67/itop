changi67/itop

commit 968b6084231aa789758feac28caa0615c872dc10

NEW! Archiving data. Archiving is a soft delete. It can be undone. Enter the archive mode to see all the data including archives (everything is read-only in that mode). Archiving must be enabled per class (data model). Archiving is achieved by the mean of the API DBObject::Archive (or Unarchive).
git-svn-id: https://svn.code.sf.net/p/itop/code/trunk@4692 a333f486-631f-4898-b8df-5754b55c2be0

application/cmdbabstract.class.inc.php
@@ -198,8 +198,6 @@ EOF
 			$oBlock->Display($oPage, -1);
 		}
 
-		$oPage->add("<div class=\"page_header\"><h1>".$this->GetIcon()."&nbsp;\n");
-
 		// Master data sources
 		$bSynchronized = false;
 		$aIcons = array();
@@ -292,16 +290,46 @@ EOF
 					$sTip .= "<p style=\"white-space:nowrap\">".$oDataSource->GetIcon(true, 'style="vertical-align:middle"')."&nbsp;$sLink<br/>";
 					$sTip .= Dict::S('Core:Synchro:LastSynchro') . '<br/>' . $sLastSynchro . "</p>";
 				}
-				$aIcons[] = '&nbsp;<img style="vertical-align:middle;" id="synchro_icon" src="../images/locked.png"/>';
+				$sLabel = htmlentities(Dict::S('Tag:Synchronized'), ENT_QUOTES, 'UTF-8');
+				$sSynchroTagId = 'synchro_icon-'.$this->GetKey();
+				$aIcons[] = "<div class=\"tag\" id=\"$sSynchroTagId\"><span class=\"object-synchronized fa fa-lock fa-1x\">&nbsp;</span>&nbsp;$sLabel</div>";
 				$sTip = addslashes($sTip);
-				$oPage->add_ready_script("$('#synchro_icon').qtip( { content: '$sTip', show: 'mouseover', hide: { fixed: true }, style: { name: 'dark', tip: 'leftTop' }, position: { corner: { target: 'rightMiddle', tooltip: 'leftTop' }} } );");
+				$oPage->add_ready_script("$('#$sSynchroTagId').qtip( { content: '$sTip', show: 'mouseover', hide: { fixed: true }, style: { name: 'dark', tip: 'topLeft' }, position: { corner: { target: 'bottomMiddle', tooltip: 'topLeft' }} } );");
 			}
 		}
-	
-		$sIcons = implode(' ', $aIcons);
-		$oPage->add(MetaModel::GetName(get_class($this)).": <span class=\"hilite\">".$this->GetName()."</span>$sIcons</h1>\n");
-		$oPage->add("</div>\n");
-		
+
+		if ($this->IsArchived())
+		{
+			$sLabel = htmlentities(Dict::S('Tag:Archived'), ENT_QUOTES, 'UTF-8');
+			$sTitle = htmlentities(Dict::S('Tag:Archived+'), ENT_QUOTES, 'UTF-8');
+			$aIcons[] = "<div class=\"tag\" title=\"$sTitle\"><span class=\"object-archived fa fa-archive fa-1x\">&nbsp;</span>&nbsp;$sLabel</div>";
+		}
+
+		$sObjectIcon = $this->GetIcon();
+		$sClassName = MetaModel::GetName(get_class($this));
+		$sObjectName = $this->GetName();
+		if (count($aIcons) > 0)
+		{
+			$sTags = '<div class="tags">'.implode('&nbsp;', $aIcons).'</div>';
+		}
+		else
+		{
+			$sTags = '';
+		}
+
+		$oPage->add(
+<<<EOF
+<div class="page_header">
+   <div class="object-details-header">
+      <div class ="object-icon">$sObjectIcon</div>
+      <div class ="object-infos">
+		  <h1 class="object-name">$sClassName: <span class="hilite">$sObjectName</span></h1>
+		  $sTags
+      </div>
+   </div>
+</div>
+EOF
+		);
 	}
 
 	function DisplayBareHistory(WebPage $oPage, $bEditMode = false, $iLimitCount = 0, $iLimitStart = 0)
@@ -558,7 +586,6 @@ EOF
 				
 				foreach($aNotificationClasses as $sNotifClass)
 				{
-					
 					$oPage->p(MetaModel::GetClassIcon($sNotifClass, true).'&nbsp;'.MetaModel::GetName($sNotifClass));
 					$oBlock = new DisplayBlock($aNotifSearches[$sNotifClass], 'list', false);
 					$oBlock->Display($oPage, 'notifications_'.$sNotifClass, array('menu' => false));
@@ -1933,7 +1960,7 @@ EOF
 					$aEventsList[] ='validate';
 					$aEventsList[] ='change';
 
-					$oAllowedValues = MetaModel::GetAllowedValuesAsObjectSet($sClass, $sAttCode, $aArgs);
+					$oAllowedValues = MetaModel::GetAllowedValuesAsObjectSet($sClass, $sAttCode, $aArgs, '', $value);
 					$sFieldName = $sFieldPrefix.$sAttCode.$sNameSuffix;
 					$aExtKeyParams = $aArgs;
 					$aExtKeyParams['iFieldSize'] = $oAttDef->GetMaxSize();
@@ -2526,7 +2553,7 @@ EOF
 					{
 						if ($oAttDef->IsExternalKey())
 						{
-							$oAllowedValues = MetaModel::GetAllowedValuesAsObjectSet($sClass, $sAttCode, $aArgs);
+							$oAllowedValues = MetaModel::GetAllowedValuesAsObjectSet($sClass, $sAttCode, $aArgs, '', $this->Get($sAttCode));
 							if ($oAllowedValues->Count() == 1)
 							{
 								$oRemoteObj = $oAllowedValues->Fetch();

application/ajaxwebpage.class.inc.php
@@ -1,5 +1,5 @@
 <?php
-// Copyright (C) 2010-2016 Combodo SARL
+// Copyright (C) 2010-2017 Combodo SARL
 //
 //   This file is part of iTop.
 //
@@ -20,7 +20,7 @@
  * Simple web page with no includes, header or fancy formatting, useful to
  * generate HTML fragments when called by an AJAX method
  *
- * @copyright   Copyright (C) 2010-2016 Combodo SARL
+ * @copyright   Copyright (C) 2010-2017 Combodo SARL
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
 
@@ -53,7 +53,11 @@ class ajax_page extends WebPage implements iTabbedPage
 		$this->sContentType = 'text/html';
 		$this->sContentDisposition = 'inline';
 		$this->m_sMenu = "";
-    }	
+
+		$bArchiveMode = utils::IsArchiveMode();
+		DBSearch::SetArchiveModeDefault($bArchiveMode);
+		if ($bArchiveMode) MetaModel::DBSetReadOnly();
+    }
 
 	public function AddTabContainer($sTabContainer, $sPrefix = '')
 	{

application/itopwebpage.class.inc.php
@@ -1,5 +1,5 @@
 <?php
-// Copyright (C) 2010-2016 Combodo SARL
+// Copyright (C) 2010-2017 Combodo SARL
 //
 //   This file is part of iTop.
 //
@@ -20,7 +20,7 @@
 /**
  * Class iTopWebPage
  *
- * @copyright   Copyright (C) 2010-2016 Combodo SARL
+ * @copyright   Copyright (C) 2010-2017 Combodo SARL
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
 
@@ -34,7 +34,7 @@ class iTopWebPage extends NiceWebPage implements iTabbedPage
 {
 	private $m_sMenu;
 	//	private $m_currentOrganization;
-	private $m_sMessage;
+	private $m_aMessages;
 	private $m_sInitScript;
 	protected $m_oTabs;
 	protected $bBreadCrumbEnabled;
@@ -63,8 +63,12 @@ class iTopWebPage extends NiceWebPage implements iTabbedPage
 			$this->bBreadCrumbEnabled = false;
 		}
 
+		$bArchiveMode = utils::IsArchiveMode();
+		DBSearch::SetArchiveModeDefault($bArchiveMode);
+		if ($bArchiveMode) MetaModel::DBSetReadOnly();
+
 		$this->m_sMenu = "";
-		$this->m_sMessage = '';
+		$this->m_aMessages = array();
 		$this->SetRootUrl(utils::GetAbsoluteUrlAppRoot());
 		$this->add_header("Content-type: text/html; charset=utf-8");
 		$this->add_header("Cache-control: no-cache");
@@ -75,6 +79,7 @@ class iTopWebPage extends NiceWebPage implements iTabbedPage
 		$this->add_linked_stylesheet("../css/jquery.multiselect.css");
 		$this->add_linked_stylesheet("../css/magnific-popup.css");
 		$this->add_linked_stylesheet("../css/c3.min.css");
+		$this->add_linked_stylesheet("../css/font-awesome/css/font-awesome.min.css");
 
 		$this->add_linked_script('../js/jquery.layout.min.js');
 		$this->add_linked_script('../js/jquery.ba-bbq.min.js');
@@ -120,6 +125,23 @@ function ShowAboutBox()
 		$('body').append(data);
 	});
 	return false;
+}
+function ArchiveMode(bEnable)
+{
+	var sPrevUrl = StripArchiveArgument(window.location.search);
+	if (bEnable)
+	{
+		window.location.search = sPrevUrl + '&with-archive=1';
+	}
+	else
+	{
+		window.location.search = sPrevUrl + '&with-archive=0';
+	}
+}
+function StripArchiveArgument(sUrl)
+{
+	var res = sUrl.replace(/&with-archive=[01]/g, '');
+	return res;
 }
 EOF
 			);
@@ -732,7 +754,7 @@ EOF
 
 		if (UserRights::IsAdministrator() && ExecutionKPI::IsEnabled())
 		{
-			$sNorthPane .= '<div id="admin-banner"><span style="padding:5px;">'.ExecutionKPI::GetDescription().'<span></div>';
+			$sNorthPane .= '<div class="app-message"><span style="padding:5px;">'.ExecutionKPI::GetDescription().'<span></div>';
 		}
 		
 		//$sSouthPane = '<p>Peak memory Usage: '.sprintf('%.3f MB', memory_get_peak_usage(true) / (1024*1024)).'</p>';
@@ -982,7 +1004,20 @@ EOF
 
 			$oPrefs = new URLPopupMenuItem('UI:Preferences', Dict::S('UI:Preferences'), utils::GetAbsoluteUrlAppRoot()."pages/preferences.php?".$oAppContext->GetForLink());
 			$aActions[$oPrefs->GetUID()] = $oPrefs->GetMenuItem();
-				
+
+			if (utils::IsArchiveMode())
+			{
+				$oExitArchive = new JSPopupMenuItem('UI:ArchiveModeOff', Dict::S('UI:ArchiveModeOff'), 'return ArchiveMode(false);');
+				$aActions[$oExitArchive->GetUID()] = $oExitArchive->GetMenuItem();
+
+				$sIcon = '<span class="fa fa-lock fa-1x"></span>';
+				$this->AddApplicationMessage(Dict::S('UI:ArchiveMode:Banner'), $sIcon, Dict::S('UI:ArchiveMode:Banner+'));
+			}
+			elseif (UserRights::CanBrowseArchive())
+			{
+				$oBrowseArchive = new JSPopupMenuItem('UI:ArchiveModeOn', Dict::S('UI:ArchiveModeOn'), 'return ArchiveMode(true);');
+				$aActions[$oBrowseArchive->GetUID()] = $oBrowseArchive->GetMenuItem();
+			}
 			if (utils::CanLogOff())
 			{
 				$oLogOff = new URLPopupMenuItem('UI:LogOffMenu', Dict::S('UI:LogOffMenu'), utils::GetAbsoluteUrlAppRoot().'pages/logoff.php?operation=do_logoff');
@@ -1014,26 +1049,34 @@ EOF
 				$sRestrictions = Dict::S('UI:AccessRO-Users');
 			}
 
-			$sApplicationBanner = '';
 			if (strlen($sRestrictions) > 0)
 			{
+				$sIcon =
+<<<EOF
+<span class="fa-stack fa-sm">
+  <i class="fa fa-pencil fa-flip-horizontal fa-stack-1x"></i>
+  <i class="fa fa-ban fa-stack-2x text-danger"></i>
+</span>
+EOF;
+
 				$sAdminMessage = trim(MetaModel::GetConfig()->Get('access_message'));
-				$sApplicationBanner .= '<div id="admin-banner">';
-				$sApplicationBanner .= '<img src="../images/locked.png" style="vertical-align:middle;">';
-				$sApplicationBanner .= '&nbsp;<b>'.$sRestrictions.'</b>';
 				if (strlen($sAdminMessage) > 0)
 				{
-					$sApplicationBanner .= '&nbsp;<b>'.$sAdminMessage.'</b>';
+					$sRestrictions .= '&nbsp;'.$sAdminMessage;
 				}
-				$sApplicationBanner .= '</div>';
+				$this->AddApplicationMessage($sRestrictions, $sIcon);
 			}
 
-			if(strlen($this->m_sMessage))
+			$sApplicationMessages = '';
+			foreach ($this->m_aMessages as $aMessage)
 			{
-				$sApplicationBanner .= '<div id="admin-banner"><span style="padding:5px;">'.$this->m_sMessage.'<span></div>';
+				$sHtmlIcon = $aMessage['icon'] ? $aMessage['icon'] : '';
+				$sHtmlMessage = $aMessage['message'];
+				$sTitleAttr = $aMessage['tip'] ? 'title="'.htmlentities($aMessage['tip'], ENT_QUOTES, 'UTF-8').'"' : '';
+				$sApplicationMessages .= '<div class="app-message" '.$sTitleAttr.'><span class="app-message-icon">'.$sHtmlIcon.'</span><span class="app-message-body">'.$sHtmlMessage.'</div></span>';
 			}
 
-			$sApplicationBanner .= $sBannerExtraHtml;
+			$sApplicationBanner = "<div class=\"app-banner ui-helper-clearfix\">$sApplicationMessages$sBannerExtraHtml</div>";
 			
 			if (!empty($sNorthPane))
 			{
@@ -1333,10 +1376,26 @@ EOF
 	}
 
 	/**
-	 * Set the message to be displayed in the 'admin-banner' section at the top of the page
+	 * Set the message to be displayed in the 'app-banner' section at the top of the page
 	 */
-	public function SetMessage($sMessage)
+	public function SetMessage($sHtmlMessage)
 	{
-			$this->m_sMessage = $sMessage;
+		$sHtmlIcon = '<span class="fa fa-comment fa-1x"></span>';
+		$this->AddApplicationMessage($sHtmlMessage, $sHtmlIcon);
+	}
+
+	/**
+	 * Add message to be displayed in the 'app-banner' section at the top of the page
+	 */
+	public function AddApplicationMessage($sHtmlMessage, $sHtmlIcon = null, $sTip = null)
+	{
+		if (strlen($sHtmlMessage))
+		{
+			$this->m_aMessages[] = array(
+				'icon' => $sHtmlIcon,
+				'message' => $sHtmlMessage,
+				'tip' => $sTip
+			);
+		}
 	}
 }

application/loginwebpage.class.inc.php
@@ -1,5 +1,5 @@
 <?php
-// Copyright (C) 2010-2016 Combodo SARL
+// Copyright (C) 2010-2017 Combodo SARL
 //
 //   This file is part of iTop.
 //
@@ -20,7 +20,7 @@
 /**
  * Class LoginWebPage
  *
- * @copyright   Copyright (C) 2010-2016 Combodo SARL
+ * @copyright   Copyright (C) 2010-2017 Combodo SARL
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
 
@@ -430,6 +430,8 @@ EOF
 		// Unset all of the session variables.
 		unset($_SESSION['auth_user']);
 		unset($_SESSION['login_mode']);
+		unset($_SESSION['archive_mode']);
+		unset($_SESSION['archive_allowed']);
 		UserRights::_ResetSessionCache();
 		// If it's desired to kill the session, also delete the session cookie.
 		// Note: This will destroy the session, and not just the session data!

application/portalwebpage.class.inc.php
@@ -1,5 +1,5 @@
 <?php
-// Copyright (C) 2010-2013 Combodo SARL
+// Copyright (C) 2010-2017 Combodo SARL
 //
 //   This file is part of iTop.
 //
@@ -19,7 +19,7 @@
 /**
  * Class PortalWebPage
  *
- * @copyright   Copyright (C) 2010-2016 Combodo SARL
+ * @copyright   Copyright (C) 2010-2017 Combodo SARL
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
 
@@ -387,7 +387,7 @@ EOF
 		{
 			$sReadOnly = Dict::S('UI:AccessRO-Users');
 			$sAdminMessage = trim(MetaModel::GetConfig()->Get('access_message'));
-			$sApplicationBanner .= '<div id="admin-banner">';
+			$sApplicationBanner .= '<div class="app-message">';
 			$sApplicationBanner .= '<img src="../images/locked.png" style="vertical-align:middle;">';
 			$sApplicationBanner .= '&nbsp;<b>'.$sReadOnly.'</b>';
 			if (strlen($sAdminMessage) > 0)

application/utils.inc.php
@@ -1,7 +1,7 @@
 <?php
 use Html2Text\Html2Text;
 use Leafo\ScssPhp\Compiler;
-// Copyright (C) 2010-2016 Combodo SARL
+// Copyright (C) 2010-2017 Combodo SARL
 //
 //   This file is part of iTop.
 //
@@ -22,7 +22,7 @@ use Leafo\ScssPhp\Compiler;
 /**
  * Static class utils
  *
- * @copyright   Copyright (C) 2010-2016 Combodo SARL
+ * @copyright   Copyright (C) 2010-2017 Combodo SARL
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
 
@@ -140,6 +140,20 @@ class utils
 		}
 	}
 
+	public static function IsArchiveMode()
+	{
+		if (isset($_SESSION['archive_mode']))
+		{
+			$iDefault = $_SESSION['archive_mode'];
+		}
+		else
+		{
+			$iDefault = 0;
+		}
+		$iCurrent = self::ReadParam('with-archive', $iDefault, true);
+		$_SESSION['archive_mode'] = $iCurrent;
+		return ($iCurrent == 1);
+	}
 
 	public static function ReadParam($sName, $defaultValue = "", $bAllowCLI = false, $sSanitizationFilter = 'parameter')
 	{
@@ -700,7 +714,7 @@ class utils
 		}
 		return $bResult;
 	}
-	
+
 	/**
 	 * Initializes the CAS client
 	 */

core/attributedef.class.inc.php
@@ -232,7 +232,8 @@ abstract class AttributeDefinition
 	public function IsExternalKey($iType = EXTKEY_RELATIVE) {return false;} 
 	public function IsHierarchicalKey() {return false;}
 	public function IsExternalField() {return false;} 
-	public function IsWritable() {return false;} 
+	public function IsWritable() {return false;}
+	public function IsMagic() {return $this->GetOptional('magic', false);}
 	public function LoadInObject() {return true;}
 	public function LoadFromDB() {return true;}
 	public function AlwaysLoadInTables() {return $this->GetOptional('always_load_in_tables', false);}
@@ -399,6 +400,7 @@ abstract class AttributeDefinition
 	public function GetSQLColumns($bFullSpec = false) {return array();} // returns column/spec pairs (1 in most of the cases), for STRUCTURING (DB creation)
 	public function GetSQLValues($value) {return array();} // returns column/value pairs (1 in most of the cases), for WRITING (Insert, Update)
 	public function RequiresIndex() {return false;}
+	public function CopyOnAllTables() {return false;}
 
 	public function GetOrderBySQLExpressions($sClassAlias)
 	{
@@ -1416,7 +1418,7 @@ class AttributeDBFieldVoid extends AttributeDefinition
 
 	public function IsDirectField() {return true;} 
 	public function IsScalar() {return true;} 
-	public function IsWritable() {return true;} 
+	public function IsWritable() {return !$this->IsMagic();}
 	public function GetSQLExpr()
 	{
 		return $this->Get("sql");
@@ -1799,7 +1801,7 @@ class AttributeBoolean extends AttributeInteger
 
 	public function GetEditClass() {return "Integer";}
 	protected function GetSQLCol($bFullSpec = false) {return "TINYINT(1)".($bFullSpec ? $this->GetSQLColSpec() : '');}
-	
+
 	public function MakeRealValue($proposedValue, $oHostObj)
 	{
 		if (is_null($proposedValue)) return null;
@@ -1814,25 +1816,169 @@ class AttributeBoolean extends AttributeInteger
 		return 0;
 	}
 
-	public function GetAsXML($sValue, $oHostObject = null, $bLocalize = true)
+	public function GetValueLabel($bValue)
 	{
-		return $sValue ? '1' : '0';
+		if (is_null($bValue))
+		{
+			$sLabel = Dict::S('Core:'.get_class($this).'/Value:null');
+		}
+		else
+		{
+			$sValue = $bValue ? 'yes' : 'no';
+			$sDefault = Dict::S('Core:'.get_class($this).'/Value:'.$sValue);
+			$sLabel = $this->SearchLabel('/Attribute:'.$this->m_sCode.'/Value:'.$sValue, $sDefault, true /*user lang*/);
+		}
+		return $sLabel;
 	}
-	public function GetAsCSV($sValue, $sSeparator = ',', $sTextQualifier = '"', $oHostObject = null, $bLocalize = true, $bConvertToPlainText = false)
+
+	public function GetValueDescription($bValue)
 	{
-		return $sValue ? '1' : '0';
+		if (is_null($bValue))
+		{
+			$sDescription = Dict::S('Core:'.get_class($this).'/Value:null+');
+		}
+		else
+		{
+			$sValue = $bValue ? 'yes' : 'no';
+			$sDefault = Dict::S('Core:'.get_class($this).'/Value:'.$sValue.'+');
+			$sDescription = $this->SearchLabel('/Attribute:'.$this->m_sCode.'/Value:'.$sValue.'+', $sDefault, true /*user lang*/);
+		}
+		return $sDescription;
 	}
-	public function GetAsHTML($sValue, $oHostObject = null, $bLocalize = true)
+
+	public function GetAsHTML($bValue, $oHostObject = null, $bLocalize = true)
 	{
-		return $sValue ? '1' : '0';
+		if (is_null($bValue))
+		{
+			$sRes = '';
+		}
+		elseif ($bLocalize)
+		{
+			$sLabel = $this->GetValueLabel($bValue);
+			$sDescription = $this->GetValueDescription($bValue);
+			// later, we could imagine a detailed description in the title
+			$sRes = "<span title=\"$sDescription\">".parent::GetAsHtml($sLabel)."</span>";
+		}
+		else
+		{
+			$sRes = $bValue ? 'yes' : 'no';
+		}
+		return $sRes;
 	}
+
+	public function GetAsXML($bValue, $oHostObject = null, $bLocalize = true)
+	{
+		if (is_null($bValue))
+		{
+			$sFinalValue = '';
+		}
+		elseif ($bLocalize)
+		{
+			$sFinalValue = $this->GetValueLabel($bValue);
+		}
+		else
+		{
+			$sFinalValue = $bValue ? 'yes' : 'no';
+		}
+		$sRes = parent::GetAsXML($sFinalValue, $oHostObject, $bLocalize);
+		return $sRes;
+	}
+
+	public function GetAsCSV($bValue, $sSeparator = ',', $sTextQualifier = '"', $oHostObject = null, $bLocalize = true, $bConvertToPlainText = false)
+	{
+		if (is_null($bValue))
+		{
+			$sFinalValue = '';
+		}
+		elseif ($bLocalize)
+		{
+			$sFinalValue = $this->GetValueLabel($bValue);
+		}
+		else
+		{
+			$sFinalValue = $bValue ? 'yes' : 'no';
+		}
+		$sRes = parent::GetAsCSV($sFinalValue, $sSeparator, $sTextQualifier, $oHostObject, $bLocalize);
+		return $sRes;
+	}
+
+	static public function GetFormFieldClass()
+	{
+		return '\\Combodo\\iTop\\Form\\Field\\SelectField';
+	}
+
+	public function MakeFormField(DBObject $oObject, $oFormField = null)
+	{
+		if ($oFormField === null)
+		{
+			$sFormFieldClass = static::GetFormFieldClass();
+			$oFormField = new $sFormFieldClass($this->GetCode());
+		}
+
+		$oFormField->SetChoices(array('yes' => $this->GetValueLabel(true), 'no' => $this->GetValueLabel(false)));
+		parent::MakeFormField($oObject, $oFormField);
+
+		return $oFormField;
+	}
+
+	public function GetEditValue($value, $oHostObj = null)
+	{
+		if (is_null($value))
+		{
+			return '';
+		}
+		else
+		{
+			return $this->GetValueLabel($value);
+		}
+	}
+
 	/**
 	 * Helper to get a value that will be JSON encoded
-	 * The operation is the opposite to FromJSONToValue	 
-	 */	 	
+	 * The operation is the opposite to FromJSONToValue
+	 */
 	public function GetForJSON($value)
 	{
-		return $value ? '1' : '0';
+		return (bool)$value;
+	}
+
+	public function MakeValueFromString($sProposedValue, $bLocalizedValue = false, $sSepItem = null, $sSepAttribute = null, $sSepValue = null, $sAttributeQualifier = null)
+	{
+		$sInput = strtolower(trim($sProposedValue));
+		if ($bLocalizedValue)
+		{
+			switch ($sInput)
+			{
+				case '1': // backward compatibility
+				case $this->GetValueLabel(true):
+					$value = true;
+					break;
+				case '0': // backward compatibility
+				case 'no':
+				case $this->GetValueLabel(false):
+					$value = false;
+					break;
+				default:
+					$value = null;
+			}
+		}
+		else
+		{
+			switch ($sInput)
+			{
+				case '1': // backward compatibility
+				case 'yes':
+					$value = true;
+					break;
+				case '0': // backward compatibility
+				case 'no':
+					$value = false;
+					break;
+				default:
+					$value = null;
+			}
+		}
+		return $value;
 	}
 }
 
@@ -2087,6 +2233,10 @@ class AttributeFinalClass extends AttributeString
 	public function IsWritable()
 	{
 		return false;
+	}
+	public function IsMagic()
+	{
+		return true;
 	}
 
 	public function RequiresIndex()
@@ -4312,10 +4462,10 @@ class AttributeExternalKey extends AttributeDBFieldVoid
 		}
 	}
 
-	public function GetAllowedValuesAsObjectSet($aArgs = array(), $sContains = '')
+	public function GetAllowedValuesAsObjectSet($aArgs = array(), $sContains = '', $iAdditionalValue = null)
 	{
 		$oValSetDef = $this->GetValuesDef();
-		$oSet = $oValSetDef->ToObjectSet($aArgs, $sContains);
+		$oSet = $oValSetDef->ToObjectSet($aArgs, $sContains, $iAdditionalValue);
 		return $oSet;
 	}
 
@@ -4535,7 +4685,7 @@ class AttributeHierarchicalKey extends AttributeExternalKey
 		}
 	}
 
-	public function GetAllowedValuesAsObjectSet($aArgs = array(), $sContains = '')
+	public function GetAllowedValuesAsObjectSet($aArgs = array(), $sContains = '', $iAdditionalValue = null)
 	{
 		$oValSetDef = $this->GetValuesDef();
 		if (array_key_exists('this', $aArgs))
@@ -4551,7 +4701,7 @@ class AttributeHierarchicalKey extends AttributeExternalKey
 				$oValSetDef->AddCondition($oFilter);
 			}
 		}
-		$oSet = $oValSetDef->ToObjectSet($aArgs, $sContains);
+		$oSet = $oValSetDef->ToObjectSet($aArgs, $sContains, $iAdditionalValue);
 		return $oSet;
 	}
 
@@ -5265,7 +5415,7 @@ class AttributeStopWatch extends AttributeDefinition
 	
 	public function IsDirectField() {return true;} 
 	public function IsScalar() {return true;} 
-	public function IsWritable() {return false;} 
+	public function IsWritable() {return true;}
 	public function GetDefaultValue(DBObject $oHostObject = null) {return $this->NewStopWatch();}
 
 	public function GetEditValue($value, $oHostObj = null)
@@ -6630,6 +6780,10 @@ class AttributeFriendlyName extends AttributeComputedFieldVoid
 	{
 		return false;
 	}
+	public function IsMagic()
+	{
+		return true;
+	}
 
 	public function IsDirectField()
 	{
@@ -7440,3 +7594,26 @@ class AttributeCustomFields extends AttributeDefinition
 	}
 }
 
+class AttributeArchiveFlag extends AttributeBoolean
+{
+	public function __construct($sCode)
+	{
+		parent::__construct($sCode, array("allowed_values" => null, "sql" => $sCode, "default_value" => false, "is_null_allowed" => false, "depends_on" => array()));
+	}
+	public function RequiresIndex()
+	{
+		return true;
+	}
+	public function CopyOnAllTables()
+	{
+		return true;
+	}
+	public function IsWritable()
+	{
+		return false;
+	}
+	public function IsMagic()
+	{
+		return true;
+	}
+}

commit 50cf3660158d82230f518fcac6e6a95754cd1a57

N.981 Regression introduced in [r4692]. It make sense to preserve the existing value... excepted when reloading some form elements (filtering depending on fields that have just been touched by the user).
git-svn-id: https://svn.code.sf.net/p/itop/code/trunk@4895 a333f486-631f-4898-b8df-5754b55c2be0

application/cmdbabstract.class.inc.php
@@ -1763,8 +1763,22 @@ EOF
 */
 		return $sHtml;
 	}
-	
-	public static function GetFormElementForField($oPage, $sClass, $sAttCode, $oAttDef, $value = '', $sDisplayValue = '', $iId = '', $sNameSuffix = '', $iFlags = 0, $aArgs = array())
+
+	/**
+	 * @param $oPage
+	 * @param $sClass
+	 * @param $sAttCode
+	 * @param $oAttDef
+	 * @param string $value
+	 * @param string $sDisplayValue
+	 * @param string $iId
+	 * @param string $sNameSuffix
+	 * @param int $iFlags
+	 * @param array $aArgs
+	 * @param bool $bPreserveCurrentValue Preserve the current value even if not allowed
+	 * @return string
+	 */
+	public static function GetFormElementForField($oPage, $sClass, $sAttCode, $oAttDef, $value = '', $sDisplayValue = '', $iId = '', $sNameSuffix = '', $iFlags = 0, $aArgs = array(), $bPreserveCurrentValue = true)
 	{
 		static $iInputId = 0;
 		$sFieldPrefix = '';
@@ -2026,7 +2040,14 @@ EOF
 					$aEventsList[] ='validate';
 					$aEventsList[] ='change';
 
-					$oAllowedValues = MetaModel::GetAllowedValuesAsObjectSet($sClass, $sAttCode, $aArgs, '', $value);
+					if ($bPreserveCurrentValue)
+					{
+						$oAllowedValues = MetaModel::GetAllowedValuesAsObjectSet($sClass, $sAttCode, $aArgs, '', $value);
+					}
+					else
+					{
+						$oAllowedValues = MetaModel::GetAllowedValuesAsObjectSet($sClass, $sAttCode, $aArgs);
+					}
 					$sFieldName = $sFieldPrefix.$sAttCode.$sNameSuffix;
 					$aExtKeyParams = $aArgs;
 					$aExtKeyParams['iFieldSize'] = $oAttDef->GetMaxSize();

pages/ajax.render.php
@@ -626,7 +626,7 @@ try
 					else
 					{
 						$iFlags = MetaModel::GetAttributeFlags($sClass, $oObj->GetState(), $sAttCode);
-						$sHTMLValue = cmdbAbstractObject::GetFormElementForField($oPage, $sClass, $sAttCode, $oAttDef, $value, $displayValue, $sId, '', $iFlags, array('this' => $oObj, 'formPrefix' => $sFormPrefix));
+						$sHTMLValue = cmdbAbstractObject::GetFormElementForField($oPage, $sClass, $sAttCode, $oAttDef, $value, $displayValue, $sId, '', $iFlags, array('this' => $oObj, 'formPrefix' => $sFormPrefix), false);
 						// Make sure that we immediately validate the field when we reload it
 						$oPage->add_ready_script("$('#$sId').trigger('validate');");
 					}